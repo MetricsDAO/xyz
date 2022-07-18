c86b865cea9578ec9d9958789b86dcdea532aa98
kakamora <86695148+thekakamora@users.noreply.github.com>
2022-07-18 00:01:04 -0700

    Introduce xMetric Dashboard

--- app/components/app-footer.tsx
+++ app/components/app-footer.tsx
@@ -7,7 +7,7 @@ export default function AppFooter() {
         <div className="row d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
           <div className="col-4 d-flex align-items-center">
             <Link to="/" className="footer-brand">
-              <img src="img/black-mark@2x.png" alt="MetricsDAO" width="96" />
+              <img src="/img/black-mark@2x.png" alt="MetricsDAO" width="96" />
             </Link>
           </div>
           <ul className="nav col-auto justify-content-end list-unstyled d-flex flex-row flex-lg-column social-network">
