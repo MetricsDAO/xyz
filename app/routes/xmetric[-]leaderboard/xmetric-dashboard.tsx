ee84c06f53a56dcd92e0edd9374d99caa3d40c95
kakamora <86695148+thekakamora@users.noreply.github.com>
2022-07-19 14:29:53 -0700

    Introduce xMetric Dashboard (#34)
    * Introduce xMetric Dashboard
    
    * Incorporate feedback

---
+++ app/routes/xmetric[-]leaderboard/xmetric-dashboard.tsx
@@ -0,0 +1,12 @@
+export default () => {  
+  return (
+    <>
+      <iframe                                
+        src="https://analytics.zoho.com/open-view/2467969000005415175"
+        width={"100%"}
+        height={2200} 
+        title="xMetric Dashboard"       
+      />
+    </>
+  )
+}
