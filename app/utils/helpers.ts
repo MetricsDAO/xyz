47faa0a7375dc28cb7c2d282f149f60bf032a2bc
gnomadic <edflem+github@gmail.com>
2022-08-11 09:37:10 -0400

    updating question state enums

--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -1,23 +1,23 @@
 import { useEffect, useRef } from "react";
-export function truncateAddress (address: string | undefined) {
-    if (address) {
-        return address.substring(0, 5) + "..." + address.substring(address.length - 5);
-    }
+export function truncateAddress(address: string | undefined) {
+  if (address) {
+    return (
+      address.substring(0, 5) + "..." + address.substring(address.length - 5)
+    );
+  }
 }
 
 export function getIcon(connectorName: string | undefined) {
-    if (connectorName) {
+  if (connectorName) {
     return WALLET_ICONS[connectorName] || WALLET_ICONS.WalletIcon;
-    }
+  }
 }
 
 const WALLET_ICONS: Record<string, string> = {
-    WalletConnect: "/img/wallet-connect-white.svg",
-    MetaMask: "/img/metamask-fox.svg",
-    WalletIcon: "img/wallet-icon.svg"
-  };
-
-
+  WalletConnect: "/img/wallet-connect-white.svg",
+  MetaMask: "/img/metamask-fox.svg",
+  WalletIcon: "img/wallet-icon.svg",
+};
 
 export function usePrevious<T>(value: T): T | undefined {
   const ref = useRef<T>();
@@ -28,43 +28,39 @@ export function usePrevious<T>(value: T): T | undefined {
 }
 
 export enum TransactionStatus {
-    Pending = "pending",
-    Approved = "approved",
-    Failed = "Failed",
-  }
+  Pending = "pending",
+  Approved = "approved",
+  Failed = "Failed",
+}
 
 export const questionStateEnum = {
-    UNINIT: 0,
-    VOTING: 1,
-    PUBLISHED: 2,
-    IN_GRADING: 3,
-    COMPLETED: 4,
-    CANCELLED: 5,
-    BAD: 6,
+  UNINIT: 0,
+  VOTING: 1,
+  PUBLISHED: 2,
+  DISQUALIFIED: 3,
+  COMPLETED: 4,
 };
 
 export const OFFSET = 1000;
 
-
-
 // TODO DO WE CHANGE
 export const sortMethods = [
   {
-    name: 'Votes',
+    name: "Votes",
   },
   {
-    name: 'Newest',
+    name: "Newest",
   },
 ];
 
 // TODO How to get protocols
 
 export const protocols = [
-  { name: 'All'},
-  { name: 'Ethereum' },
-  { name: 'Flow' },
-  { name: 'Algorand' },
-  { name: 'THORchain' },
-  { name: 'Cosmos' },
-  { name: 'Polygon' },
-]
+  { name: "All" },
+  { name: "Ethereum" },
+  { name: "Flow" },
+  { name: "Algorand" },
+  { name: "THORchain" },
+  { name: "Cosmos" },
+  { name: "Polygon" },
+];
