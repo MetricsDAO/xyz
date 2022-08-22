f4ac9d30c9dc34f6e0f859cbf98cca02b5167fad
gnomadic <edflem+github@gmail.com>
2022-08-22 13:27:39 -0400

    iterating, breaks on createQuestion

--- app/components/AllQuestionsContainer.tsx
+++ app/components/AllQuestionsContainer.tsx
@@ -1,59 +1,59 @@
 import { useEffect, useState } from "react";
-import { useContractRead } from 'wagmi';
+import { useContractRead } from "wagmi";
 import { BigNumber } from "ethers";
 
 import AllQuestionsByState from "~/components/AllQuestionsByState";
 
+export default function AllQuestionContainer({
+  address,
+  questionAPI,
+  xmetric,
+  questionStateController,
+  bountyQuestion,
+  networkMatchesWallet,
+}: {
+  address?: string;
+  questionAPI: Record<string, string>;
+  xmetric?: Record<string, string>;
+  questionStateController: Record<string, string>;
+  bountyQuestion: Record<string, string>;
+  networkMatchesWallet: boolean;
+}) {
+  const [latestTokenId, setLatestTokenId] = useState<number>(0);
 
-export default function AllQuestionContainer ({
-    address, 
-    questionAPI, 
-    xmetric, 
-    questionStateController,
-    bountyQuestion,
-    networkMatchesWallet
-    }:{
-    address?: string, 
-    questionAPI: Record<string, string>, 
-    xmetric?: Record<string, string>, 
-    questionStateController: Record<string, string>,
-    bountyQuestion: Record<string, string>,
-    networkMatchesWallet: boolean
-    }) {
+  const { data: currentQuestion } = useContractRead(
+    {
+      addressOrName: bountyQuestion.address,
+      contractInterface: bountyQuestion.abi,
+    },
+    "getMostRecentQuestion",
+    {
+      onError: (err) => {
+        console.log(bountyQuestion.address);
+        console.error(err);
+      },
+    }
+  );
 
-    const [latestTokenId, setLatestTokenId] = useState<number>(0);
-    
+  useEffect(() => {
+    if (BigNumber.isBigNumber(currentQuestion)) {
+      // console.log("latest question ID", currentQuestion.toNumber());
+      setLatestTokenId(currentQuestion.toNumber());
+    }
+  }, [currentQuestion]);
 
-    const {data: currentQuestion} = useContractRead({
-        addressOrName: bountyQuestion.address,
-        contractInterface: bountyQuestion.abi,
-    }, 'getMostRecentQuestion', {
-        onError: (err) => {
-          console.error(err);
-        },
-    });
-
-
-    useEffect(() => {
-        if (BigNumber.isBigNumber(currentQuestion)) {
-            // console.log("latest question ID", currentQuestion.toNumber());
-            setLatestTokenId(currentQuestion.toNumber());
-        }
-    }, [currentQuestion])
-
-    return (
-            <>
-            <section className="tw-mx-auto tw-mb-7 tw-container tw-max-w-screen-xl">
-              {latestTokenId && (
-                <AllQuestionsByState 
-                  questionAPI={questionAPI} 
-                  latestQuestion={latestTokenId} 
-                  questionStateController={questionStateController}
-                  networkMatchesWallet={networkMatchesWallet} 
-                />
-              )}
-            </section>
-            </>
-
-    )
+  return (
+    <>
+      <section className="tw-mx-auto tw-mb-7 tw-container tw-max-w-screen-xl">
+        {latestTokenId && (
+          <AllQuestionsByState
+            questionAPI={questionAPI}
+            latestQuestion={latestTokenId}
+            questionStateController={questionStateController}
+            networkMatchesWallet={networkMatchesWallet}
+          />
+        )}
+      </section>
+    </>
+  );
 }
--- app/components/NetworkRender.tsx
+++ app/components/NetworkRender.tsx
@@ -2,43 +2,60 @@ import React from "react";
 import type { ReactElement } from "react";
 
 export default function NetworkRender({
-    network, 
-    chainName, 
-    chainId, 
-    switchNetwork,
-    children
-    }:{
-    network: string,
-    chainName?: string, 
-    chainId?: number, 
-    switchNetwork?: (chainId?:number) => void,
-    children?: ReactElement
+  network,
+  chainName,
+  chainId,
+  switchNetwork,
+  children,
+}: {
+  network: string;
+  chainName?: string;
+  chainId?: number;
+  switchNetwork?: (chainId?: number) => void;
+  children?: ReactElement;
 }) {
-    function getContents() {
-        if (network !== chainName?.toLowerCase()) {
-            if (typeof(switchNetwork) === "function") {
-                return (
-                <div className={`tw-flex tw-flex-wrap tw-max-w-xs tw-text-sm tw-mx-auto ${children ? "tw-justify-center" : ""}`}>
-                    <p className="tw-mb-2"> You are currently connected to {chainName}</p>
-                    <button onClick={() => switchNetwork(chainId)} className="tw-bg-[#21C5F2] tw-px-5 tw-py-3 tw-text-sm tw-rounded-lg tw-text-white">switch network</button>
-                </div>
-                )
-            } else {
-                return (
-                <div className={`tw-flex tw-flex-wrap tw-max-w-xs tw-text-sm tw-mx-auto ${children} ? "tw-justify-center" : ""`}>
-                    <p className="tw-mb-2"> You are currently connected to {chainName}</p>
-                    <p>Please switch wallet to {network} network</p>
-                </div>
-                )
-            }
-        } else {
-            if (children) {
-                return children;
-            } else {
-                return null;
-            }
-        }
+  function getContents() {
+    console.log("network", network, "chainName", chainName, "chainId", chainId);
+    if (network !== chainName?.toLowerCase()) {
+      if (typeof switchNetwork === "function") {
+        return (
+          <div
+            className={`tw-flex tw-flex-wrap tw-max-w-xs tw-text-sm tw-mx-auto ${
+              children ? "tw-justify-center" : ""
+            }`}
+          >
+            <p className="tw-mb-2">
+              {" "}
+              You are currently connected to {chainName}
+            </p>
+            <button
+              onClick={() => switchNetwork(chainId)}
+              className="tw-bg-[#21C5F2] tw-px-5 tw-py-3 tw-text-sm tw-rounded-lg tw-text-white"
+            >
+              switch network
+            </button>
+          </div>
+        );
+      } else {
+        return (
+          <div
+            className={`tw-flex tw-flex-wrap tw-max-w-xs tw-text-sm tw-mx-auto ${children} ? "tw-justify-center" : ""`}
+          >
+            <p className="tw-mb-2">
+              {" "}
+              You are currently connected to {chainName}
+            </p>
+            <p>Please switch wallet to {network} network</p>
+          </div>
+        );
+      }
+    } else {
+      if (children) {
+        return children;
+      } else {
+        return null;
+      }
     }
-    return getContents();
-
+  }
+  return getContents();
 }
