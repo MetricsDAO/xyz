e0c8397f98b4db4a5bfabfcac12fa99fde52857f
gnomadic <edflem+github@gmail.com>
2022-08-23 16:39:51 -0400

    network switcher for hardhat and polygon

--- app/components/WalletProvider.tsx
+++ app/components/WalletProvider.tsx
@@ -1,13 +1,6 @@
 import type { ReactElement } from "react";
 import { ClientOnly } from "remix-utils";
-import {
-  WagmiConfig,
-  createClient,
-  configureChains,
-  chain,
-  defaultChains,
-  defaultL2Chains,
-} from "wagmi";
+import { WagmiConfig, createClient, configureChains, chain, defaultChains, defaultL2Chains } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 // import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from "wagmi/connectors/injected";
@@ -15,29 +8,25 @@ import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { infuraProvider } from "wagmi/providers/infura";
 
-export default function WalletProvider({
-  children,
-  network,
-}: {
-  children: ReactElement;
-  network: string;
-}) {
+export default function WalletProvider({ children, network }: { children: ReactElement; network: string }) {
   let configureChainObj;
 
   //TODO create config of all chains we support
 
   // TODO configure for POLYGON
 
-  if (network === "ropsten") {
-    const infuraId = "54fcc811bac44f99b84a04a4a3e2f998";
-    configureChainObj = configureChains(defaultChains, [
-      infuraProvider({ infuraId }),
-      publicProvider(),
-    ]);
-  } else {
-    const infuraId = "54fcc811bac44f99b84a04a4a3e2f998";
+  const infuraId = "54fcc811bac44f99b84a04a4a3e2f998";
+
+  if (network === "ropsten" || network == "goerli") {
     configureChainObj = configureChains(
-      [chain.polygon],
+      [chain.ropsten, chain.goerli],
+      [infuraProvider({ infuraId }), publicProvider()]
+    );
+  } else if (network == "polygon") {
+    configureChainObj = configureChains([chain.polygon], [infuraProvider({ infuraId }), publicProvider()]);
+  } else {
+    configureChainObj = configureChains(
+      [chain.localhost, chain.hardhat],
       [infuraProvider({ infuraId }), publicProvider()]
     );
   }
