234ba76d0ca4939f6868b67b69d50059f3ad52b0
James Daly <jaimiedaly@gmail.com>
2022-08-26 12:18:49 -0500

    css and link updates

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,73 +1,70 @@
 import { Link } from "remix";
-import { useRef } from 'react';
+import { useRef } from "react";
 import SocialIcons from "~/components/SocialIcons";
 
 const Header = () => {
-    const navRef = useRef<any>(null);
-    const menuRef = useRef<any>(null);
+  const navRef = useRef<any>(null);
+  const menuRef = useRef<any>(null);
 
-    const toggleMenu = () => {
-        let nav = navRef.current;
-        let hamburger = menuRef.current;
-        if (nav.classList.contains("nav-open")) {
-            nav.classList.remove("nav-open");
-            hamburger.classList.remove("hamburger-close");
-        } else {
-            nav.classList.add("nav-open");
-            hamburger.classList.add("hamburger-close");
-        }
-    };
+  const toggleMenu = () => {
+    let nav = navRef.current;
+    let hamburger = menuRef.current;
+    if (nav.classList.contains("nav-open")) {
+      nav.classList.remove("nav-open");
+      hamburger.classList.remove("hamburger-close");
+    } else {
+      nav.classList.add("nav-open");
+      hamburger.classList.add("hamburger-close");
+    }
+  };
 
-    return (
-        <header className="tw-flex tw-flex-wrap tw-justify-between tw-items-center tw-absolute lg:tw-static tw-w-full tw-left-0">
-            <div className="tw-w-full lg:tw-w-1/4 tw-flex tw-items-center tw-justify-center lg:tw-justify-start">
-                <Link
-                    to="/"
-                    className="tw-no-underline"
-                >
-                    <img
-                        src="/img/bw-lightbg@2x.png"
-                        alt="MetricsDAO"
-                        width="241"
-                        height="44"
-                    />
-                </Link>
-            </div>
-            <div ref={menuRef} className="hamburger tw-p-3 tw-space-y-1 tw-rounded tw-shadow lg:tw-hidden
-             tw-cursor-pointer tw-absolute tw-right-2" onClick={toggleMenu}>
-                <span className="tw-block tw-w-6 tw-h-0.5 tw-bg-gray-600"></span>
-                <span className="tw-block tw-w-6 tw-h-0.5 tw-bg-gray-600"></span>
-                <span className="tw-block tw-w-6 tw-h-0.5 tw-bg-gray-600"></span>
-            </div>
-            <ul ref={navRef} className="nav tw-w-3/4 tw-justify-end tw-list-none tw-hidden lg:tw-flex tw-items-center">
-                <Link
-                    className="btn btn-outline-dark rounded-pill xl:tw-px-4 lg:tw-px-3 xl:tw-mr-4 lg:tw-mr-2 md:tw-mr-1"
-                    to="/dashboard"
-                >
-                    DASHBOARD
-                </Link>
-                <Link
-                    className="btn btn-outline-dark rounded-pill xl:tw-px-4 lg:tw-px-3 xl:tw-mr-4 lg:tw-mr-2 md:tw-mr-1"
-                    to="/showcase"
-                >
-                    SHOWCASE
-                </Link>
-                <a
-                    className="btn btn-outline-dark rounded-pill xl:tw-px-4 lg:tw-px-3 xl:tw-mr-4 lg:tw-mr-2 md:tw-mr-1"
-                    href="https://metricsdao.ghost.io/"
-                >
-                    BLOG
-                </a>
-                <a
-                    className="btn btn-outline-dark rounded-pill xl:tw-px-4 lg:tw-px-3 xl:tw-mr-4 lg:tw-mr-2 md:tw-mr-1 text-uppercase"
-                    href="https://partnerwith.metricsdao.xyz"
-                >
-                    Partner with Us
-                </a>
-                <SocialIcons/>
-            </ul>
-        </header>
-    )
-}
+  return (
+    <header className="tw-flex tw-flex-wrap tw-justify-between tw-items-center tw-absolute lg:tw-static tw-w-full tw-left-0">
+      <div className="tw-w-full lg:tw-w-1/4 tw-flex tw-items-center tw-justify-center lg:tw-justify-start">
+        <Link to="/" className="tw-no-underline">
+          <img src="/img/bw-lightbg@2x.png" alt="MetricsDAO" width="241" height="44" />
+        </Link>
+      </div>
+      <div
+        ref={menuRef}
+        className="hamburger tw-p-3 tw-space-y-1 tw-rounded tw-shadow lg:tw-hidden
+             tw-cursor-pointer tw-absolute tw-right-2"
+        onClick={toggleMenu}
+      >
+        <span className="tw-block tw-w-6 tw-h-0.5 tw-bg-gray-600"></span>
+        <span className="tw-block tw-w-6 tw-h-0.5 tw-bg-gray-600"></span>
+        <span className="tw-block tw-w-6 tw-h-0.5 tw-bg-gray-600"></span>
+      </div>
+      <ul ref={navRef} className="nav tw-w-3/4 tw-justify-end tw-list-none tw-hidden lg:tw-flex tw-items-center">
+        <Link
+          className="btn btn-outline-dark rounded-pill xl:tw-px-4 lg:tw-px-3 xl:tw-mr-4 lg:tw-mr-2 md:tw-mr-1"
+          to="/dashboard"
+        >
+          DASHBOARD
+        </Link>
+        <Link
+          className="btn btn-outline-dark rounded-pill xl:tw-px-4 lg:tw-px-3 xl:tw-mr-4 lg:tw-mr-2 md:tw-mr-1"
+          to="/showcase"
+        >
+          SHOWCASE
+        </Link>
+        <a
+          className="btn btn-outline-dark rounded-pill xl:tw-px-4 lg:tw-px-3 xl:tw-mr-4 lg:tw-mr-2 md:tw-mr-1"
+          href="https://metricsdao.ghost.io/"
+        >
+          BLOG
+        </a>
+        {/* TODO FIX AS AOS not working unless page is refreshed */}
+        <a
+          className="btn btn-outline-dark rounded-pill xl:tw-px-4 lg:tw-px-3 xl:tw-mr-4 lg:tw-mr-2 md:tw-mr-1 text-uppercase"
+          href="https://metricsdao.xyz/3-step-process"
+        >
+          Partner with Us
+        </a>
+        <SocialIcons />
+      </ul>
+    </header>
+  );
+};
 
 export default Header;
