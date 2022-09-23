f713110358575073c6b2ffec2be98b9ae29009c8
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-09-23 14:45:33 -0400

    useContracts improvement

--- app/hooks/useContracts.ts
+++ app/hooks/useContracts.ts
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react";
 import type { Chain } from "wagmi";
 import { useContract, useNetwork, useProvider } from "wagmi";
+import * as PolygonxMetricJson from "core-evm-contracts/deployments/polygon/Xmetric.json";
+import * as PolygonQuestionAPIJson from "core-evm-contracts/deployments/polygon/QuestionAPI.json";
+import * as PolygonQuestionStateControllerJson from "core-evm-contracts/deployments/polygon/QuestionStateController.json";
+import * as PolygonBountyQuestionJson from "core-evm-contracts/deployments/polygon/BountyQuestion.json";
+import * as PolygonCostControllerJson from "core-evm-contracts/deployments/polygon/ActionCostController.json";
+import * as PolygonVaultJson from "core-evm-contracts/deployments/polygon/Vault.json";
+import invariant from "invariant";
 
 export function useContracts({ chainId }: { chainId?: Chain["id"] }) {
   const [
@@ -30,32 +37,16 @@ function getContracts(chainId?: Chain["id"]) {
   let costController;
   let vaultJson;
 
-  if (chainId === 137 || !chainId) {
-    xMetricJson = require(`core-evm-contracts/deployments/polygon/Xmetric.json`);
-    questionAPIJson = require(`core-evm-contracts/deployments/polygon/QuestionAPI.json`);
-    questionStateController = require(`core-evm-contracts/deployments/polygon/QuestionStateController.json`);
-    bountyQuestionJson = require(`core-evm-contracts/deployments/polygon/BountyQuestion.json`);
-    costController = require(`core-evm-contracts/deployments/polygon/ActionCostController.json`);
-    vaultJson = require(`core-evm-contracts/deployments/polygon/Vault.json`);
-  }
-  // // TODO: What is going on with the other chains?? Missing contracts?
-  // if (chainId === 3) {
-  //   xMetricJson = require(`core-evm-contracts/deployments/ropsten/Xmetric.json`);
-  //   questionAPIJson = require(`core-evm-contracts/deployments/ropsten/QuestionAPI.json`);
-  //   questionStateController = require(`core-evm-contracts/deployments/ropsten/QuestionStateController.json`);
-  //   bountyQuestionJson = require(`core-evm-contracts/deployments/ropsten/BountyQuestion.json`);
-  //   costController = require(`core-evm-contracts/deployments/ropsten/ActionCostController.json`);
-  //   vaultJson = require(`core-evm-contracts/deployments/ropsten/Vault.json`);
-  // }
-  //   else {
-  //     //localhost
-  //     xMetricJson = require(`core-evm-contracts/deployments/localhost/Xmetric.json`);
-  //     questionAPIJson = require(`core-evm-contracts/deployments/localhost/QuestionAPI.json`);
-  //     questionStateController = require(`core-evm-contracts/deployments/localhost/QuestionStateController.json`);
-  //     bountyQuestionJson = require(`core-evm-contracts/deployments/localhost/BountyQuestion.json`);
-  //     costController = require(`core-evm-contracts/deployments/localhost/ActionCostController.json`);
-  //     vaultJson = require(`core-evm-contracts/deployments/localhost/Vault.json`);
-  //   }
+  // if (chainId === 137) {
+  // Polygon
+  xMetricJson = PolygonxMetricJson;
+  questionAPIJson = PolygonQuestionAPIJson;
+  questionStateController = PolygonQuestionStateControllerJson;
+  bountyQuestionJson = PolygonBountyQuestionJson;
+  costController = PolygonCostControllerJson;
+  vaultJson = PolygonVaultJson;
+
+  // More chains to come?...
 
   return {
     xMetricJson,
@@ -71,6 +62,7 @@ export function useBountyQuestionContract() {
   const { chain } = useNetwork();
   const provider = useProvider({ chainId: chain?.id });
   const { bountyQuestionJson } = useContracts({ chainId: chain?.id });
+  invariant(bountyQuestionJson, "BountyQuestion contract not found");
   const contract = useContract({
     addressOrName: bountyQuestionJson.address,
     contractInterface: bountyQuestionJson.abi,
@@ -83,6 +75,7 @@ export function useQuestionStateControllerContract() {
   const { chain } = useNetwork();
   const provider = useProvider({ chainId: chain?.id });
   const { questionStateController } = useContracts({ chainId: chain?.id });
+  invariant(questionStateController, "QuestionStateController contract not found");
   const contract = useContract({
     addressOrName: questionStateController.address,
     contractInterface: questionStateController.abi,
