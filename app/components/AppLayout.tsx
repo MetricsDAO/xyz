2cb036f94875a89146e7a875804d8c8b53739bfc
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-10-02 13:48:44 -0400

    small layout improvement

--- app/components/AppLayout.tsx
+++ app/components/AppLayout.tsx
@@ -1,9 +1,22 @@
+// Responsive layout for body content. Left and right panels are optional.
+
+/**
+ *    <Layout>
+        <Layout.Content>
+          <p>Content<p>
+        </Layout.Content>
+        <Layout.RightPanel>
+          <p>Right Panel<p>
+        </Layout.RightPanel>
+      </Layout>
+ *
+ */
+
 // Smaller screens need less padding on top
 const sectionPadding = "tw-pt-5 md:tw-pt-10 tw-px-6";
-
 export function Layout({ children }: { children: React.ReactNode }) {
   // Column on mobile, row on desktop
-  return <main className="tw-flex tw-flex-col md:tw-flex-row">{children}</main>;
+  return <main className="tw-flex tw-flex-col lg:tw-flex-row tw-justify-center">{children}</main>;
 }
 
 // eslint-disable-next-line react/display-name
