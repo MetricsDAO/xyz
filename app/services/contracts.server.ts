7c04bcb2f5a44aa09cebe007ffe0c618f9b6b1fb
James Daly <jaimiedaly@gmail.com>
2022-10-03 15:55:54 -0500

    testing dynamic require

--- app/services/contracts.server.ts
+++ app/services/contracts.server.ts
@@ -6,29 +6,36 @@ export function getContracts({ network }: { network: string }) {
   let costController;
   let vaultJson;
 
-  if (network === "ropsten") {
-    xMetricJson = require(`core-evm-contracts/deployments/ropsten/Xmetric.json`);
-    questionAPIJson = require(`core-evm-contracts/deployments/ropsten/QuestionAPI.json`);
-    questionStateController = require(`core-evm-contracts/deployments/ropsten/QuestionStateController.json`);
-    bountyQuestionJson = require(`core-evm-contracts/deployments/ropsten/BountyQuestion.json`);
-    costController = require(`core-evm-contracts/deployments/ropsten/ActionCostController.json`);
-    vaultJson = require(`core-evm-contracts/deployments/ropsten/Vault.json`);
-  } else if (network === "polygon") {
-    xMetricJson = require(`core-evm-contracts/deployments/polygon/Xmetric.json`);
-    questionAPIJson = require(`core-evm-contracts/deployments/polygon/QuestionAPI.json`);
-    questionStateController = require(`core-evm-contracts/deployments/polygon/QuestionStateController.json`);
-    bountyQuestionJson = require(`core-evm-contracts/deployments/polygon/BountyQuestion.json`);
-    costController = require(`core-evm-contracts/deployments/polygon/ActionCostController.json`);
-    vaultJson = require(`core-evm-contracts/deployments/polygon/Vault.json`);
-  } else {
-    //localhost
-    xMetricJson = require(`core-evm-contracts/deployments/localhost/Xmetric.json`);
-    questionAPIJson = require(`core-evm-contracts/deployments/localhost/QuestionAPI.json`);
-    questionStateController = require(`core-evm-contracts/deployments/localhost/QuestionStateController.json`);
-    bountyQuestionJson = require(`core-evm-contracts/deployments/localhost/BountyQuestion.json`);
-    costController = require(`core-evm-contracts/deployments/localhost/ActionCostController.json`);
-    vaultJson = require(`core-evm-contracts/deployments/localhost/Vault.json`);
-  }
+  // if (network === "ropsten") {
+  //   xMetricJson = require(`core-evm-contracts/deployments/ropsten/Xmetric.json`);
+  //   questionAPIJson = require(`core-evm-contracts/deployments/ropsten/QuestionAPI.json`);
+  //   questionStateController = require(`core-evm-contracts/deployments/ropsten/QuestionStateController.json`);
+  //   bountyQuestionJson = require(`core-evm-contracts/deployments/ropsten/BountyQuestion.json`);
+  //   costController = require(`core-evm-contracts/deployments/ropsten/ActionCostController.json`);
+  //   vaultJson = require(`core-evm-contracts/deployments/ropsten/Vault.json`);
+  // } else if (network === "polygon") {
+  //   xMetricJson = require(`core-evm-contracts/deployments/polygon/Xmetric.json`);
+  //   questionAPIJson = require(`core-evm-contracts/deployments/polygon/QuestionAPI.json`);
+  //   questionStateController = require(`core-evm-contracts/deployments/polygon/QuestionStateController.json`);
+  //   bountyQuestionJson = require(`core-evm-contracts/deployments/polygon/BountyQuestion.json`);
+  //   costController = require(`core-evm-contracts/deployments/polygon/ActionCostController.json`);
+  //   vaultJson = require(`core-evm-contracts/deployments/polygon/Vault.json`);
+  // } else {
+  //   //localhost
+  //   xMetricJson = require(`core-evm-contracts/deployments/localhost/Xmetric.json`);
+  //   questionAPIJson = require(`core-evm-contracts/deployments/localhost/QuestionAPI.json`);
+  //   questionStateController = require(`core-evm-contracts/deployments/localhost/QuestionStateController.json`);
+  //   bountyQuestionJson = require(`core-evm-contracts/deployments/localhost/BountyQuestion.json`);
+  //   costController = require(`core-evm-contracts/deployments/localhost/ActionCostController.json`);
+  //   vaultJson = require(`core-evm-contracts/deployments/localhost/Vault.json`);
+  // }
+
+  xMetricJson = require(`core-evm-contracts/deployments/${network}/Xmetric.json`);
+  questionAPIJson = require(`core-evm-contracts/deployments/${network}/QuestionAPI.json`);
+  questionStateController = require(`core-evm-contracts/deployments/${network}/QuestionStateController.json`);
+  bountyQuestionJson = require(`core-evm-contracts/deployments/${network}/BountyQuestion.json`);
+  costController = require(`core-evm-contracts/deployments/${network}/ActionCostController.json`);
+  vaultJson = require(`core-evm-contracts/deployments/${network}/Vault.json`);
 
   return {
     xMetricJson: {
