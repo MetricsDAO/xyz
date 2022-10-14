ac79404b11c2d801d3dc43f1924da4b6212c95c2
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-10-14 11:23:56 -0700

    fixing eslint implementation

--- app/routes/xmetric[-]leaderboard/xmetric-dashboard.tsx
+++ app/routes/xmetric[-]leaderboard/xmetric-dashboard.tsx
@@ -1,12 +1,12 @@
-export default () => {  
+export default () => {
   return (
     <>
-      <iframe                                
+      <iframe
         src="https://analytics.zoho.com/open-view/2467969000005415175"
         width={"100%"}
-        height={2200} 
-        title="xMetric Dashboard"       
+        height={2200}
+        title="xMetric Dashboard"
       />
     </>
-  )
-}
+  );
+};
