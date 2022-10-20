b234884e52fce61dddecf50a3dcc063b146d87ec
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-10-20 12:19:31 -0700

    mantine

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,9 @@
+import { createStylesServer, injectStyles } from "@mantine/remix";
+import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
-import type { EntryContext } from "@remix-run/react/entry";
 import { renderToString } from "react-dom/server";
+
+const server = createStylesServer();
 
 export default function handleRequest(
   request: Request,
@@ -9,10 +12,9 @@ export default function handleRequest(
   remixContext: EntryContext
 ) {
   let markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
-
   responseHeaders.set("Content-Type", "text/html");
 
-  return new Response("<!DOCTYPE html>" + markup, {
+  return new Response(`<!DOCTYPE html>${injectStyles(markup, server)}`, {
     status: responseStatusCode,
     headers: responseHeaders,
   });
