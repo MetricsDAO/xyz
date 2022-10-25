005a4e8b13c8a2763101b271c6cf826df1e061a2
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2022-10-25 14:03:26 -0400

    Basic components added to create marketplace

--- app/components/UpdateMarketplace.tsx
+++ app/components/UpdateMarketplace.tsx
@@ -1,44 +1,66 @@
+import { MultiSelect, NativeSelect, NumberInput, Textarea, TextInput } from "@mantine/core";
+
 export function UpdateMarketplace() {
+  const delegateCreate = true;
+
   return (
-    <div className="space-y-7 bg-gray-200 p-3">
-      <i className="text-xl font-semibold">{"Create/Update Marketplace"}</i>
-      <input placeholder="Title" className=" text-black p-3 rounded-lg border w-full" />
-      <div className="space-y-5">
-        <input placeholder="Descriptions" className="text-black p-3 rounded-lg border w-full" />
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          <label htmlFor="creator" className="text-black self-center">
-            Who can create topics:
-          </label>
-          <input id="creator" placeholder="Start" className="p-3 rounded-lg border w-full" />
-        </div>
-        <input placeholder="Badger contact address" className="text-black p-3 rounded-lg border w-full" />
-        <input placeholder="Token ID" className="text-black p-3 rounded-lg border w-full" />
-      </div>
-      <div className="space-y-3">
-        <input placeholder="Project/Chain (multiselect)" className="text-black p-3 rounded-lg border w-full" />
-        <input placeholder="Author reward tokens (multiselect)" className="text-black p-3 rounded-lg border w-full" />
-        <input placeholder="Author reward curve" className="text-black p-3 rounded-lg border w-full" />
-      </div>
-      <div className="space-y-3">
+    <div className="space-y-3 p-3">
+      <b className="text-3xl font-semibold">{"Create/Update Marketplace"}</b>
+      <p>
+        Brainstorm marketplaces empower our community to crowdsource the best questions for crypto analysts to answer.
+        Create a marketplace for you and peers to host, incentivize, and engage in brainstorms for any web3 topic.
+      </p>
+      <TextInput label="Title" placeholder="Marketplace name" className=" text-black w-full" />
+      <Textarea
+        label="Details"
+        placeholder="Enter text here..."
+        className="text-black w-full md:col-span-2"
+        autosize
+        minRows={3}
+        maxRows={5}
+      />
+      <MultiSelect
+        data={["React", "Angular", "Svelte", "Vue"]}
+        label="Blockchian/Project"
+        placeholder="Choose one or more Bloockchain/Project"
+      />
+      <NativeSelect
+        className="text-black"
+        label="Control who can create topics"
+        data={["React", "Angular", "Svelte", "Vue"]}
+      />
+      {delegateCreate ? (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-y-3 gap-x-5">
-          <p className="md:col-span-2">{"Author rep requirement (optional)"}</p>
-          <input placeholder="Min" className="p-3 rounded-lg border" />
-          <input placeholder="Max" className="p-3 rounded-lg border" />
+          <p className="text-sm md:col-span-2">Delegates</p>
+          <TextInput placeholder="Badger Contact Address" className=" text-black w-full" />
+          <TextInput placeholder="Token ID" className=" text-black w-full" />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-y-3 gap-x-5">
-          <p className="md:col-span-2">{"Reviewer rep requirement (optional)"}</p>
-          <input placeholder="Min" className="p-3 rounded-lg border" />
-          <input placeholder="Max" className="p-3 rounded-lg border" />
-        </div>
+      ) : (
+        <></>
+      )}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-3 gap-x-5">
+        <b className="md:col-span-2">Rewards</b>
+        <NativeSelect
+          label="Allow Reward Token"
+          data={["React", "Angular", "Svelte", "Vue"]}
+          placeholder={"Reward Token"}
+        />
+        <NativeSelect
+          label="Reward Curve"
+          data={["React", "Angular", "Svelte", "Vue"]}
+          placeholder={"Aggressive: Rewards top 10%"}
+        />
+        <b className="md:col-span-2">{"Control who can submit questions (Optional)"}</b>
+        <NumberInput placeholder="xMetric Min" hideControls />
+        <NumberInput placeholder="xMetric Max" hideControls />
+        <b className="md:col-span-2">{"Control who can peer review questions (Default)"}</b>
+        <TextInput placeholder="{Reviewer Badger Contact Address}" className=" text-black w-full" />
+        <TextInput placeholder="Token ID" className=" text-black w-full" />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-y-3 gap-x-2">
-        <p className="md:col-span-3">{"Reviewer rep priority"}</p>
-        <input placeholder="Cheap" className="p-3 rounded-lg border" />
-        <input placeholder="Average" className="p-3 rounded-lg border" />
-        <input placeholder="Aggressive" className="p-3 rounded-lg border" />
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-5 items-center mt-10">
+        <button className="bg-[#16ABDD] text-white rounded-md p-3 font-medium">Create</button>
+        <button className="bg-white text-black border rounded-md p-3 font-medium">Cancel</button>
       </div>
-      <p>Explaner on selected priority</p>
-      <div className="bg-black text-white p-3 rounded-lg w-1/2 text-center">Save Marketplace</div>
     </div>
   );
 }
