f7e9e495f7e89eeeae24a51e66e2410a443239d7
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2022-10-26 16:22:08 -0400

    Change to mantine and make boxes slightly larger

--- app/routes/app/brainstorm/$marketplace.topic.new.tsx
+++ app/routes/app/brainstorm/$marketplace.topic.new.tsx
@@ -1,4 +1,4 @@
-import { Select, TextInput, Textarea, NativeSelect, NumberInput, Button } from "@mantine/core";
+import { Select, TextInput, Textarea, NativeSelect, NumberInput, Text, Title, Button } from "@mantine/core";
 import { DatePicker, TimeInput } from "@mantine/dates";
 
 export default function CreateTopic() {
@@ -7,34 +7,35 @@ export default function CreateTopic() {
   return (
     <div className="mx-auto max-w-3xl space-y-10">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 items-center">
-        <b className="text-3xl font-semibold md:col-span-3">{"Create Topic"}</b>
-        <p className="md:col-span-3">
+        <Title order={2} weight={600} className="md:col-span-3">
+          {"Create Topic"}
+        </Title>
+        <Text className="md:col-span-3">
           Crowdsource the best questions for crypto analysts to answer about an important or timely topic. Create and
           incentivize a question brainstorm for any web3 topic to get started.
-        </p>
+        </Text>
         <Select
           className="text-black rounded-lg basis-full md:col-span-2"
           data={["React", "Angular", "Svelte", "Vue"]}
           placeholder={"Select a marketplace (w/search)"}
+          size="md"
           searchable
           clearable
         />
         <div className="flex flex-row items-center space-x-5">
-          <p>or</p>
-          <button
-            className="bg-white text-black border border-[#16ABDD] rounded-md p-3 font-medium"
-            disabled={marketplace}
-          >
+          <Text>or</Text>
+          <Button variant="outline" color="cyan.3" size="md" radius="md" disabled={marketplace}>
             Create New Marketplace
-          </button>
+          </Button>
         </div>
       </div>
       {marketplace ? (
         <div className="space-y-10">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-5 w-full">
-            <TextInput label="Title" placeholder="Name" className=" text-black w-full md:col-span-2" />
+            <TextInput label="Title" placeholder="Name" size="md" className=" text-black w-full md:col-span-2" />
             <Textarea
               label="Details"
+              size="md"
               placeholder="Enter text here..."
               className="text-black w-full md:col-span-2"
               autosize
@@ -43,47 +44,56 @@ export default function CreateTopic() {
             />
             <NativeSelect
               className="text-black"
+              size="md"
               data={["React", "Angular", "Svelte", "Vue"]}
               placeholder={"Language"}
             />
             <NativeSelect
               className="text-black"
+              size="md"
               data={["React", "Angular", "Svelte", "Vue"]}
               placeholder={"Blockchain/Project"}
             />
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-y-3 gap-x-5">
-            <b className="md:col-span-2">When can questions be submitted</b>
-            <DatePicker placeholder="Start date" className="w-full" />
-            <TimeInput format="12" className="w-full" />
-            <DatePicker placeholder="End date" className="w-full" />
-            <TimeInput format="12" className="w-full" />
-            <i className="md:col-span-2 text-[#858582]">
+            <Text weight={500} className="md:col-span-2">
+              When can questions be submitted
+            </Text>
+            <DatePicker placeholder="Start date" size="md" className="w-full" />
+            <TimeInput format="12" size="md" className="w-full" />
+            <DatePicker placeholder="End date" size="md" className="w-full" />
+            <TimeInput format="12" size="md" className="w-full" />
+            <Text italic className="md:col-span-2 text-[#858582]">
               {"Authors must claim this topic by {local timestamp} to submit question ideas"}
-            </i>
+            </Text>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-y-3 gap-x-5">
-            <b className="md:col-span-2">When must peer review be complete by</b>
-            <DatePicker placeholder="Set date" className="w-full" />
-            <TimeInput format="12" className="w-full" />
-            <i className="md:col-span-2 text-[#858582]">
+            <Text weight={500} className="md:col-span-2">
+              When must peer review be complete by
+            </Text>
+            <DatePicker placeholder="Set date" size="md" className="w-full" />
+            <TimeInput format="12" size="md" className="w-full" />
+            <Text italic className="md:col-span-2 text-[#858582]">
               {"Reviewers must claim this topic by {local timestamp} to score questions"}
-            </i>
+            </Text>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-y-3 gap-x-5">
-            <b className="md:col-span-2">Rewards</b>
+            <Text weight={500} size="lg" className="md:col-span-2">
+              Rewards
+            </Text>
             <NativeSelect
               className="text-black"
               label="Reward Token"
+              size="md"
               data={["React", "Angular", "Svelte", "Vue"]}
               placeholder={"Token"}
             />
-            <NumberInput defaultValue={1} placeholder="Amount" label="Reward Pool" hideControls />
-            <i className="md:col-span-2 text-[#858582]">
+            <NumberInput defaultValue={1} placeholder="Amount" size="md" label="Reward Pool" hideControls />
+            <Text italic className="md:col-span-2 text-[#858582]">
               {
                 "Rewards will be distributed to eligible authors via the {Reward Curve} reward curve set for the marketplace"
               }
-            </i>
+            </Text>
           </div>
         </div>
       ) : (
@@ -92,8 +102,12 @@ export default function CreateTopic() {
 
       <div>
         <div className="grid grid-cols-1 md:grid-cols-5 gap-5 items-center mt-2">
-          <button className="bg-[#16ABDD] text-white rounded-md p-3 font-medium">Submit</button>
-          <button className="bg-white text-black border rounded-md p-3 font-medium">Cancel</button>
+          <Button size="md" color="cyan">
+            Submit
+          </Button>
+          <Button variant="default" color="dark" size="md">
+            Cancel
+          </Button>
         </div>
       </div>
     </div>
