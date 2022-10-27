c83d6984c33d097d70955591d3a7fb3eee5ba2ce
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-10-27 09:33:14 -0700

    asdjasd

--- app/routes/app/brainstorm/index.tsx
+++ app/routes/app/brainstorm/index.tsx
@@ -31,13 +31,17 @@ export default function Brainstorm() {
 
   return (
     <div className="mx-auto container">
-      <section className="flex flex-col space-y-7 md:flex-row md:space-y-0 py-12">
+      <section className="flex flex-col md:flex-row space-y-7 md:space-y-0 space-x-0 md:space-x-5 py-12">
         <main className="flex-1">
           <div className="space-y-3">
             <Title order={2}>Brainstorm Marketplaces</Title>
             <Text color="dimmed" className="max-w-2xl">
-              Brainstorm marketplaces empower our community to crowdsource the best questions for crypto analysts to
-              answer. Participants can host, incentivize, and engage in brainstorms for any web3 topic.
+              <Text span color="blue">
+                Brainstorm marketplaces empower our community to host brainstorm challenges that crowdsource the best
+                questions for crypto analysts to answer.
+              </Text>{" "}
+              Ways to participate: Sponsor brainstorm challenges for any web3 topic. Submit your best question ideas on
+              challenges that interest you. Review peers’ question ideas to surface and reward challenge winners.
             </Text>
           </div>
         </main>
@@ -60,10 +64,14 @@ export default function Brainstorm() {
         <Divider />
       </section>
 
-      <section className="flex flex-col-reverse space-y-reverse space-y-7 md:space-y-0 md:flex-row">
+      <section className="flex flex-col-reverse md:flex-row space-y-reverse space-y-7 md:space-y-0 space-x-0 md:space-x-5">
         <main className="flex-1">
-          <MarketplacesGrid marketplaces={marketplaces} />
-          <Pagination page={pageNumber} onChange={onPaginationChange} total={totalPages} />
+          <div className="space-y-5">
+            <MarketplacesTable marketplaces={marketplaces} />
+            <div className="w-fit m-auto">
+              <Pagination page={pageNumber} onChange={onPaginationChange} total={totalPages} />
+            </div>
+          </div>
         </main>
         <aside className="md:w-1/5">
           <Form className="space-y-5" onChange={handleChange} ref={formRef}>
@@ -82,27 +90,63 @@ export default function Brainstorm() {
   );
 }
 
-function MarketplacesGrid({ marketplaces }: { marketplaces: Marketplace[] }) {
+function MarketplacesTable({ marketplaces }: { marketplaces: Marketplace[] }) {
   return (
-    <>
-      <div className="flex overflow-auto">
-        <div className="basis-2/6">Title</div>
-        <div className="basis-1/6">Chain/Project</div>
-        <div className="basis-1/6">Reward Pool</div>
-        <div className="basis-1/6">Entry to Submit</div>
-        <div className="basis-1/6">Topics</div>
+    <div className="overflow-auto">
+      <div className="min-w-[350px] w-full border-spacing-4 border-separate">
+        <div className="flex items-center space-x-2 text-left px-4 text-[#666666]">
+          <div className="w-2/6 font-normal overflow-hidden text-ellipsis">Brainstorm</div>
+          <div className="w-1/6 font-normal overflow-hidden text-ellipsis">Chain/Project</div>
+          <div className="w-1/6 font-normal overflow-hidden text-ellipsis">Potential Rewards</div>
+          <div className="w-1/6 font-normal overflow-hidden text-ellipsis">Entry to Submit</div>
+          <div className="w-1/6 font-normal overflow-hidden text-ellipsis"># Challenges</div>
+        </div>
+        <div className="space-y-4">
+          {marketplaces.map((m) => {
+            return (
+              <div
+                className="flex space-x-2 border-solid border-2 border-[#EDEDED] py-5 px-4 rounded-lg hover:border-black"
+                key={m.id}
+              >
+                <div className="w-2/6">{m.title}</div>
+                <div className="w-1/6">{m.project}</div>
+                <div className="w-1/6">{m.rewardPool} USD</div>
+                <div className="w-1/6">{m.entryCost} xMetric</div>
+                <div className="w-1/6">{m.topicCount}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      {marketplaces.map((m) => {
-        return (
-          <div className="flex overflow-auto" key={m.id}>
-            <div className="basis-2/6">{m.title}</div>
-            <div className="basis-1/6">{m.project}</div>
-            <div className="basis-1/6">{m.rewardPool}</div>
-            <div className="basis-1/6">{m.entryCost}</div>
-            <div className="basis-1/6">{m.topicCount}</div>
-          </div>
-        );
-      })}
-    </>
+    </div>
+  );
+
+  return (
+    <div className="overflow-auto">
+      <table className="min-w-[350px] w-full border-spacing-4 border-separate">
+        <thead>
+          <tr className="text-left px-4 text-[#666666]">
+            <th className="basis-2/6 font-normal">Brainstorm</th>
+            <th className="basis-1/6 font-normal">Chain/Project</th>
+            <th className="basis-1/6 font-normal">Potential Rewards</th>
+            <th className="basis-1/6 font-normal">Entry to Submit</th>
+            <th className="basis-1/6 font-normal"># Challenges</th>
+          </tr>
+        </thead>
+        <tbody className="space-y-4">
+          {marketplaces.map((m) => {
+            return (
+              <tr className="border-solid border-2 border-[#EDEDED] py-5 px-4 rounded-lg hover:border-black" key={m.id}>
+                <td className="basis-2/6">{m.title}</td>
+                <td className="basis-1/6">{m.project}</td>
+                <td className="basis-1/6">{m.rewardPool} USD</td>
+                <td className="basis-1/6">{m.entryCost} xMetric</td>
+                <td className="basis-1/6">{m.topicCount}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
