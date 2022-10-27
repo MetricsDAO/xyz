2971ae0f35ea229441f42d09916bba6454dfe9d3
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-10-27 13:31:19 -0700

    removing Mantine ClientProvider from entry.client.tsx

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,17 +1,11 @@
-import { hydrate } from "react-dom";
 import { RemixBrowser } from "@remix-run/react";
-import { ClientProvider } from "@mantine/remix";
 
 // Polyfills https://github.com/rainbow-me/rainbowkit/blob/main/examples/with-remix/app/polyfills.ts
 import { Buffer } from "buffer-polyfill";
+import { hydrate } from "react-dom";
 
 window.global = window.global ?? window;
 window.Buffer = window.Buffer ?? Buffer;
 window.process = window.process ?? { env: {} }; // Minimal process polyfill
 
-hydrate(
-  <ClientProvider>
-    <RemixBrowser />
-  </ClientProvider>,
-  document
-);
+hydrate(<RemixBrowser />, document);
