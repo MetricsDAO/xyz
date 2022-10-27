8a1aed183b125fb5203dbaeb12e1f73ffe61c94f
Bryan Cinman <bcinman@fastmail.com>
2022-10-27 01:27:51 -0400

    styling tweaks

--- app/components/InfoCard.tsx
+++ app/components/InfoCard.tsx
@@ -1,9 +1,13 @@
-import { Title } from "@mantine/core";
+import { Paper, Title } from "@mantine/core";
 import clsx from "clsx";
 
 export function InfoCard({ className, children }: { children: React.ReactNode; className?: string }) {
-  const classes = clsx("flex flex-col space-y-3 border rounded-lg p-6", className);
-  return <div className={classes}>{children}</div>;
+  const classes = clsx("flex flex-col space-y-3 rounded-lg p-6", className);
+  return (
+    <Paper withBorder className={classes}>
+      {children}
+    </Paper>
+  );
 }
 
 InfoCard.Title = function Info({ children }: { children: React.ReactNode }) {
