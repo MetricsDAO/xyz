899ed03350ca4fbae09f76ac0e67c95ce0854af0
Bryan Cinman <bcinman@fastmail.com>
2022-11-07 19:26:49 -0500

    remove vercel config

--- remix.config.js
+++ remix.config.js
@@ -3,12 +3,13 @@
  */
 module.exports = {
   serverDependenciesToBundle: ["@rainbow-me/rainbowkit", "@rainbow-me/rainbowkit/wallets"],
-  serverBuildTarget: "vercel",
+  // serverBuildTarget: "vercel",
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: !process.env.VERCEL || process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  // server: !process.env.VERCEL || process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: [".*"],
+  cacheDirectory: "./node_modules/.cache/remix",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
