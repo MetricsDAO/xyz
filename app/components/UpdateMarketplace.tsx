3bbf36aeea9e23f234139c64be40218806b74c78
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-11-08 13:58:58 -0800

    much better

--- app/components/UpdateMarketplace.tsx
+++ app/components/UpdateMarketplace.tsx
@@ -1,15 +1,15 @@
-import { MultiSelect, NativeSelect, NumberInput, Textarea, TextInput, Title, Text, Button } from "@mantine/core";
-import { useNavigate } from "@remix-run/react";
-import { useWaitForTransaction } from "wagmi";
-import { useCreateMarketplace } from "~/hooks/useCreateMarketplace";
+import { Button, Text, TextInput, Title } from "@mantine/core";
 import { useForm, zodResolver } from "@mantine/form";
+import { useNavigate } from "@remix-run/react";
+import { BigNumber } from "ethers";
+import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
 import type { LaborMarketNew } from "~/domain";
 import { LaborMarketNewSchema } from "~/domain";
-import { useState } from "react";
+
+const DEV_TEST_CONTRACT_ADDRESS = "0xd138D0B4F007EA66C8A8C0b95E671ffE788aa6A9";
 
 export function UpdateMarketplace({ title }: { title: string }) {
   const navigate = useNavigate();
-  const [marketplace, setMarketplace] = useState<LaborMarketNew>();
 
   const form = useForm<LaborMarketNew>({
     initialValues: {
@@ -17,24 +17,44 @@ export function UpdateMarketplace({ title }: { title: string }) {
     },
     validate: zodResolver(LaborMarketNewSchema),
   });
-  const { data, write } = useCreateMarketplace(marketplace);
+
+  const { config } = usePrepareContractWrite({
+    address: DEV_TEST_CONTRACT_ADDRESS,
+    abi: [
+      {
+        inputs: [{ internalType: "uint256", name: "_num", type: "uint256" }],
+        name: "test",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+    ],
+    functionName: "test",
+    enabled: form.isValid(),
+    args: form.isValid() ? [BigNumber.from(form.values.title.charCodeAt(0))] : [BigNumber.from(0)], //mocking
+  });
+
+  const { data, write } = useContractWrite(config);
+
   const { isLoading, isSuccess } = useWaitForTransaction({
     hash: data?.hash,
+    onError(error) {
+      console.log("error", error);
+    },
     onSuccess(data) {
+      console.log("success", data);
       // 1. (for test/dev) create marketplace in Prisma
       // 2. Navigate to details page? Or to marketplace list?
       navigate("/app/brainstorm");
     },
   });
 
-  const onSubmit = form.onSubmit((values) => {
-    setMarketplace(values);
-    // Race condition here between setting the marketplace and the write call?
+  const onCreate = () => {
     write?.();
-  });
+  };
 
   return (
-    <form onSubmit={onSubmit} className="space-y-7 p-3 max-w-3xl mx-auto">
+    <form className="space-y-7 p-3 max-w-3xl mx-auto">
       <p>data {JSON.stringify(data)}</p>
       <p>Is loading {JSON.stringify(isLoading)}</p>
       <p>is Success {JSON.stringify(isSuccess)}</p>
@@ -54,7 +74,7 @@ export function UpdateMarketplace({ title }: { title: string }) {
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-5 items-center">
-        <Button size="md" color="cyan" type="submit">
+        <Button size="md" color="cyan" type="button" onClick={onCreate}>
           Create
         </Button>
         <Button variant="default" color="dark" size="md">
