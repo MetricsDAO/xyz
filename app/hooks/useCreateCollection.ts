f9df4d1cc04dc1d4a26e8c34575d2005db6ab164
Bryan Cinman <bcinman@fastmail.com>
2022-11-09 02:28:23 -0500

    get typecheck working

--- app/hooks/useCreateCollection.ts
+++
@@ -1,6 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import type { Program } from "~/domain";
-
-export function useCreateCollection() {
-  return useMutation((collection: Program) => {});
-}
