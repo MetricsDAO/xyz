78f8ca7a95e90550b13fa01221861c9e597b8e5b
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-11-16 17:48:31 -0800

    avatar enhancements. Ladle story.

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,23 +1,35 @@
+import { UserAvatarFilledAlt32 } from "@carbon/icons-react";
+import { clsx } from "@mantine/core";
 import * as RadixAvatar from "@radix-ui/react-avatar";
 
 export default function Avatar({
   src,
   alt,
-  size,
+  size = "md",
   delay,
   fallback,
 }: {
   src: string;
   alt: string;
-  size: number;
+  size?: "sm" | "md" | "lg";
   delay?: number;
   fallback?: React.ReactNode;
 }) {
+  const fallBackHeight = size === "lg" ? 64 : size === "md" ? 48 : 32;
   return (
-    <RadixAvatar.Root className={`w-${size} h-${size}`}>
-      <RadixAvatar.Image className="w-full h-full" src={src} alt={alt} />
-      <RadixAvatar.Fallback className="w-full h-full" delayMs={delay}>
-        {fallback}
+    <RadixAvatar.Root>
+      <RadixAvatar.Image
+        className={clsx(
+          { "w-8 h-8": size === "sm" },
+          { "w-12 h-12": size === "md" },
+          { "w-16 h-16": size === "lg" },
+          "rounded-full"
+        )}
+        src={src}
+        alt={alt}
+      />
+      <RadixAvatar.Fallback delayMs={delay ?? 500}>
+        {fallback ?? <UserAvatarFilledAlt32 height={fallBackHeight} width={fallBackHeight} />}
       </RadixAvatar.Fallback>
     </RadixAvatar.Root>
   );
