9b90fb0c906f5440b409083d75639184f019841b
Bryan Cinman <bcinman@fastmail.com>
2022-11-17 00:29:27 -0500

    feat: challeng form

--- app/root.tsx
+++ app/root.tsx
@@ -41,7 +41,7 @@ export const links: LinksFunction = () => {
     },
     {
       rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap",
+      href: "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap",
     },
     {
       rel: "apple-touch-icon",
