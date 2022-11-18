0549ecfcfdabd2e0d96d93424c855a95afca784d
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-11-18 10:01:37 -0800

    remove 404 styling hack

--- app/routes/app/$.tsx
+++
@@ -1,6 +0,0 @@
-// Prevent Mantine styles from being removed when reaching a 404 page (https://github.com/remix-run/remix/issues/1136#issuecomment-1255452202)
-export async function loader() {
-  throw new Response("Not Found", {
-    status: 404,
-  });
-}
