f077c026729c62e736e6a13a8d1c50e52fb901c3
Bryan Cinman <bcinman@fastmail.com>
2022-11-20 01:27:22 -0500

    more icons

--- app/components/ProjectBadge.tsx
+++ app/components/ProjectBadge.tsx
@@ -1,10 +1,21 @@
 import type { Project } from "@prisma/client";
 import { Badge } from "./Badge";
+import * as Avatar from "@radix-ui/react-avatar";
 
 export function ProjectBadge({ project }: { project: Project }) {
   return (
     <Badge className="pl-2">
-      <img src={`/logos/${project.slug}.svg`} alt={`${project.name} logo`} className="h-4 w-4" />
+      <Avatar.Root className="h-5 w-5 items-center justify-center rounded-full bg-gray-200">
+        <Avatar.Image
+          src={`/img/icons/project-icons/${project.slug}.svg`}
+          alt={`${project.name} logo`}
+          className="object-contain"
+        />
+        <Avatar.Fallback className="w-full h-full flex items-center justify-center text-xs text-gray-100 bg-gray-400 rounded-full">
+          {project.slug.at(0)?.toUpperCase()}
+        </Avatar.Fallback>
+      </Avatar.Root>
+      {/* <img src={`/logos/${project.slug}.svg`} alt={`${project.name} logo`} className="h-4 w-4" /> */}
       <span className="mx-1">{project.name}</span>
     </Badge>
   );
--- app/components/TokenBadge.tsx
+++ app/components/TokenBadge.tsx
@@ -1,10 +1,14 @@
 import type { Token } from "@prisma/client";
+import * as Avatar from "@radix-ui/react-avatar";
 import { Badge } from "./Badge";
 
 export function TokenBadge({ token, value }: { token: Token; value: number }) {
   return (
     <Badge>
-      <img src={`/logos/${token.symbol}.svg`} alt={`${token.name} logo`} className="h-4 w-4" />
+      <Avatar.Root className="h-4 w-4">
+        <Avatar.Image src={`/logos/${token.symbol}.svg`} alt={`${token.name} logo`} />
+        <Avatar.Fallback>{token.symbol.at(0)}</Avatar.Fallback>
+      </Avatar.Root>
       <span className="mx-1">
         {value} {token.name}
       </span>
