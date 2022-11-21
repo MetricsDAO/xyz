50ec373c5189d819158dce40589e07387fd8a25c
Bryan Cinman <bcinman@fastmail.com>
2022-11-20 23:26:19 -0500

    minor styling fixes (#158)

--- app/components/Badge.tsx
+++ app/components/Badge.tsx
@@ -1,18 +1,25 @@
 import { Slot } from "@radix-ui/react-slot";
 import clsx from "clsx";
 
-type Props = {
+export type BadgeProps = {
   asChild?: boolean;
   className?: string;
   children: React.ReactNode;
+  variant?: "default" | "transparent";
 };
 
-export function Badge({ asChild, className, ...props }: Props) {
+const variantStyles = {
+  default: "bg-gray-100",
+  transparent: "bg-transparent",
+};
+
+export function Badge({ asChild, className, variant = "default", ...props }: BadgeProps) {
   const Comp = asChild ? Slot : "span";
   return (
     <Comp
       className={clsx(
-        "inline-flex items-center justify-center text-sm text-gray-700 bg-zinc-100 rounded-full px-3 h-8 w-fit whitespace-nowrap",
+        "inline-flex items-center justify-center text-sm text-gray-700 rounded-full px-3 h-8 w-fit whitespace-nowrap",
+        variantStyles[variant],
         className
       )}
       {...props}
