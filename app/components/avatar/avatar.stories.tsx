896c1e08703641d8a426abaae8e0763dce730f0f
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-11-22 15:54:25 -0800

    Avatar group

--- app/components/avatar/avatar.stories.tsx
+++ app/components/avatar/avatar.stories.tsx
@@ -3,9 +3,9 @@ import { Avatar } from "./avatar";
 export const Fallback = () => {
   return (
     <div className="flex flex-col gap-5">
-      <Avatar />
-      <Avatar />
-      <Avatar />
+      <Avatar size="sm" />
+      <Avatar size="md" fallback={"f"} />
+      <Avatar size="lg" fallback={"F"} />
     </div>
   );
 };
@@ -17,5 +17,15 @@ export const Basic = () => {
       <Avatar src="https://avatars.githubusercontent.com/u/11708259?v=4" size="md" />
       <Avatar src="https://avatars.githubusercontent.com/u/11708259?v=4" size="lg" />
     </div>
+  );
+};
+
+export const Group = () => {
+  return (
+    <Avatar.Group>
+      <Avatar src="https://dev.metricsdao.xyz/img/icons/project-icons/polygon.svg" size="lg" />
+      <Avatar src="https://dev.metricsdao.xyz/img/icons/project-icons/ethereum.svg" size="lg" />
+      <Avatar src="https://dev.metricsdao.xyz/img/icons/project-icons/avalanche.svg" size="lg" />
+    </Avatar.Group>
   );
 };
--- app/components/avatar/avatar.tsx
+++ app/components/avatar/avatar.tsx
@@ -4,6 +4,8 @@ import clsx from "clsx";
 type Props = {
   src?: string;
   size?: "sm" | "md" | "lg";
+  alt?: string;
+  fallback?: React.ReactNode;
 };
 
 const sizeStyles = {
@@ -13,15 +15,19 @@ const sizeStyles = {
   lg: "w-8 h-8",
 };
 
-export function Avatar({ src, size = "md" }: Props) {
+export function Avatar({ src, alt, fallback, size = "md" }: Props) {
   const classes = clsx("inline-block rounded-full bg-neutral-300 overflow-hidden", sizeStyles[size]);
   return (
     <RadixAvatar.Root className={classes}>
-      <RadixAvatar.Image className="w-full h-full" src={src} alt="avatar" />
-      <RadixAvatar.Fallback
-        className="w-full h-full flex items-center justify-center"
-        delayMs={500}
-      ></RadixAvatar.Fallback>
+      <RadixAvatar.Image className="w-full h-full" src={src} alt={alt ?? "avatar"} />
+      <RadixAvatar.Fallback className="w-full h-full flex items-center justify-center" delayMs={500}>
+        {fallback}
+      </RadixAvatar.Fallback>
     </RadixAvatar.Root>
   );
 }
+
+Avatar.Group = function AvatarGroup({ children }: { children: React.ReactNode }) {
+  const classes = clsx("-space-x-3 flex items-center");
+  return <div className={classes}>{children}</div>;
+};
