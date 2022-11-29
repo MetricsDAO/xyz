d958ae6eefd65336e0403e75114b72ccb3a67dd5
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2022-11-29 15:07:27 -0800

    prepare for using dev chains like Goerli

--- app/components/ConnectButton.tsx
+++ app/components/ConnectButton.tsx
@@ -1,93 +1,76 @@
 import Jazzicon, { jsNumberForAddress } from "react-jazzicon";
 import { useAccount, useDisconnect } from "wagmi";
-import { useEffect, useState } from "react";
-import { logger } from "ethers";
+import { useEffect } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useChainalysisContract } from "~/hooks/useChainalysisContract";
 import { Button } from "~/components/button";
 
-function CustomConnectButton() {
+function useSanctionScreening() {
   const { disconnect } = useDisconnect();
+  const account = useAccount();
 
-  const [connected, setConnected] = useState(false);
-
-  const account = useAccount({
-    onConnect({ address }) {
-      logger.info(address + " successfully connected");
-      setConnected(true);
-    },
-  });
-
-  const { data: isSanctionedAddress } = useChainalysisContract(account.address ? account.address : "");
+  const { data: isSanctionedAddress } = useChainalysisContract(account.address ?? "");
 
   useEffect(() => {
-    if (isSanctionedAddress === true && connected) {
+    if (isSanctionedAddress === true) {
       disconnect();
-      setConnected(false);
-      logger.info("address " + account.address + " was sanctioned and therefore disconnected");
+      alert("address " + account.address + " was sanctioned and therefore disconnected");
     }
-  }, [connected, disconnect, account.address, isSanctionedAddress]);
+  }, [disconnect, account.address, isSanctionedAddress]);
+}
+
+// https://www.rainbowkit.com/docs/custom-connect-button
+function CustomConnectButton() {
+  useSanctionScreening();
 
   return (
     <ConnectButton.Custom>
       {({ account, chain, openAccountModal, openChainModal, openConnectModal, authenticationStatus, mounted }) => {
-        if (isSanctionedAddress === true) {
-          authenticationStatus = "unauthenticated";
-        }
-
         const ready = mounted && authenticationStatus !== "loading";
         const connected =
           ready && account && chain && (!authenticationStatus || authenticationStatus === "authenticated");
 
+        if (!ready) {
+          // The docs make this a blank state with opactiy 0, but might be better to show a loading indicator of some sort
+          return (
+            <Button variant="gradient" className="pointer-events-none select-none opacity-0">
+              Connect Wallet
+            </Button>
+          );
+        }
+
+        if (!connected) {
+          return (
+            <Button variant="gradient" onClick={openConnectModal}>
+              Connect Wallet
+            </Button>
+          );
+        }
+
+        if (chain.unsupported) {
+          return (
+            <Button variant="danger" onClick={openChainModal}>
+              Switch Chain
+            </Button>
+          );
+        }
+
         return (
-          <div
-            {...(!ready && {
-              "aria-hidden": true,
-              style: {
-                opacity: 0,
-                pointerEvents: "none",
-                userSelect: "none",
-              },
-            })}
-          >
-            {(() => {
-              if (authenticationStatus === "unauthenticated") {
-                return <Button onClick={openConnectModal}>Sanctioned Address</Button>;
-              } else if (!connected) {
-                return (
-                  <Button variant="gradient" onClick={openConnectModal}>
-                    Connect Wallet
-                  </Button>
-                );
-              }
-
-              if (chain.unsupported || chain.name == "Ethereum") {
-                return (
-                  <Button variant="danger" onClick={openChainModal}>
-                    Switch to Polygon
-                  </Button>
-                );
-              }
-
-              return (
-                <div>
-                  <Button onClick={openAccountModal} variant="gradient">
-                    <div className="flex flex-row gap-2 items-center align-middle">
-                      {account.ensAvatar ? (
-                        account.ensAvatar
-                      ) : (
-                        <div className="rounded-full">
-                          <Jazzicon diameter={20} seed={jsNumberForAddress(account.address)} />
-                        </div>
-                      )}
-                      {account.ensName ? account.ensName : account.displayName}
-                      {/* //TODO: later we will show metric and xmetric balances
+          <div>
+            <Button onClick={openAccountModal} variant="gradient">
+              <div className="flex flex-row gap-2 items-center align-middle">
+                {account.ensAvatar ? (
+                  account.ensAvatar
+                ) : (
+                  <div className="rounded-full">
+                    <Jazzicon diameter={20} seed={jsNumberForAddress(account.address)} />
+                  </div>
+                )}
+                {account.ensName ? account.ensName : account.displayName}
+                {/* //TODO: later we will show metric and xmetric balances
                       {account.displayBalance ? ` (${account.displayBalance})` : ""} */}
-                    </div>
-                  </Button>
-                </div>
-              );
-            })()}
+              </div>
+            </Button>
           </div>
         );
       }}
