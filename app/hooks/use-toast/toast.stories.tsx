221550689614d649151163d7087556c044ee697e
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2022-12-01 15:58:50 -0600

    functional toast

---
+++ app/hooks/use-toast/toast.stories.tsx
@@ -0,0 +1,39 @@
+import { Button } from "../../components/button";
+import { Toaster } from "react-hot-toast";
+import { WarningAltFilled20 } from "@carbon/icons-react";
+import { useToast } from "./use-toast";
+
+const BasicToast = () => {
+  const handleCustom = () => {
+    useToast(
+      <div className="flex items-center">
+        <WarningAltFilled20 />
+        <p>Ope, something went wrong</p>
+      </div>
+    );
+  };
+
+  const handleString = () => {
+    useToast("Here is your toast. I like toast. Do you like toast?");
+  };
+
+  return (
+    <div className="space-x-2">
+      <Button size="sm" onClick={handleString}>
+        String
+      </Button>
+      <Button size="sm" onClick={handleCustom}>
+        Custom
+      </Button>
+    </div>
+  );
+};
+
+export const Basic = () => {
+  return (
+    <>
+      <Toaster />
+      <BasicToast />
+    </>
+  );
+};
