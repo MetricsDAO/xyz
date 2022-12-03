2b711dc14363447765acc2b16f023a48e4869555
Bryan Cinman <bcinman@fastmail.com>
2022-12-02 23:56:43 -0500

    new schema for labormarkets and testing

---
+++ app/domain/labor-market.test.ts
@@ -0,0 +1,48 @@
+import { faker } from "@faker-js/faker";
+import type { LaborMarketNew } from "./labor-market";
+import { LaborMarketNewSchema } from "./labor-market";
+
+test("LaborMarketNewSchema with launchAccess == delegates required launchBadgerAddress and launchBadgerTokenId", () => {
+  const laborMarket: LaborMarketNew = {
+    title: "Test",
+    description: "Test",
+    type: "brainstorm",
+    submitRepMin: 1,
+    submitRepMax: 100,
+    rewardCurveAddress: faker.finance.ethereumAddress(),
+    reviewBadgerAddress: faker.finance.ethereumAddress(),
+    reviewBadgerTokenId: "foo",
+    tokenSymbols: ["METRIC"],
+    projectIds: ["1"],
+    // @ts-expect-error - so we can simulate a bad input from the client
+    launch: {
+      access: "delegates",
+    },
+  };
+  const result = LaborMarketNewSchema.safeParse(laborMarket);
+  expect(result.success).toBe(false);
+  assert(result.success === false);
+  expect(result.error.issues.length).toEqual(2);
+});
+
+test("LaborMarketNew valid", () => {
+  const validLaborMarket: LaborMarketNew = {
+    title: "Test",
+    description: "Test",
+    type: "brainstorm",
+    submitRepMin: 1,
+    submitRepMax: 100,
+    rewardCurveAddress: faker.finance.ethereumAddress(),
+    reviewBadgerAddress: faker.finance.ethereumAddress(),
+    reviewBadgerTokenId: "foo",
+    tokenSymbols: ["METRIC"],
+    projectIds: ["1"],
+    launch: {
+      access: "delegates",
+      badgerAddress: faker.finance.ethereumAddress(),
+      badgerTokenId: "1",
+    },
+  };
+  const result = LaborMarketNewSchema.safeParse(validLaborMarket);
+  expect(result.success).toBe(true);
+});
