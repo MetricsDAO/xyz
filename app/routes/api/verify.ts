6bbec98cdc0709379282a49c76c63525090a8c93
Hannah Lerman <43100701+thehannahlerman@users.noreply.github.com>
2022-12-06 16:26:31 -0500

    use web3 for chainalysis gating

--- app/routes/api/verify.ts
+++ app/routes/api/verify.ts
@@ -4,12 +4,26 @@ import { unprocessableEntity } from "remix-utils";
 import { SiweMessage } from "siwe";
 import { createUserSession, getNonce } from "~/services/session.server";
 import { createUser, findUserByAddress } from "~/services/user.server";
+import chainalysisAbi from "~/abi/chainalysis.json";
+import Web3 from "web3";
+import type { AbiItem } from "web3-utils";
+
+const RPC_URL = "https://mainnet.infura.io/v3/54fcc811bac44f99b84a04a4a3e2f998";
 
 export const action: ActionFunction = async (data: DataFunctionArgs) => {
   const { message, signature } = await data.request.json();
   const siweMessage = new SiweMessage(message);
   const fields = await siweMessage.validate(signature);
 
+  //Chainalysis gating through web3.js to check if an address is sanctioned
+  const web3 = new Web3(RPC_URL);
+  const contract_address = "0x40c57923924b5c5c5455c48d93317139addac8fb";
+  const contract = new web3.eth.Contract(chainalysisAbi.abi as AbiItem[], contract_address);
+  const isSanctioned = await contract.methods.isSanctioned(message.address).call();
+
+  if (isSanctioned) {
+    return unprocessableEntity({ message: "Sanctioned Address" });
+  }
   const nonce = await getNonce(data.request);
   if (fields.nonce !== nonce) {
     return unprocessableEntity({ message: "Invalid nonce" });
