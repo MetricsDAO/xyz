75d1a277669a27a0a94ccc44b10836df52965dae
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2022-12-06 14:54:56 -0600

    Very basic outline

--- app/routes/app/ecosystem.tsx
+++ app/routes/app/ecosystem.tsx
@@ -1,7 +1,24 @@
+import { Container } from "~/components/Container";
+
 export default function Ecosystem() {
   return (
-    <div>
-      <h1>Ecosystem stats</h1>
-    </div>
+    <Container className="py-16 px-10">
+      <section className="space-y-2 max-w-3xl mb-16">
+        <h1 className="text-3xl font-semibold">Ecosystem</h1>
+        <div>
+          <p className="text-lg text-cyan-500">
+            Discover top submissions, rMETRIC holders, participants, and ecosystem metrics
+          </p>
+          <p className="text-gray-500 text-sm">Quickly surface relevant challenge activity and metrics over time</p>
+        </div>
+      </section>
+      <p>buttons</p>
+      <section>
+        <p>Activity</p>
+        <p>Participants</p>
+        <p>Rewards</p>
+        <p>Reputation</p>
+      </section>
+    </Container>
   );
 }
