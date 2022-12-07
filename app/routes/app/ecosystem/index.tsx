d08db7a747509be183a04fa9bae154a8685a306f
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2022-12-07 16:55:22 -0600

    Add some mobile

---
+++ app/routes/app/ecosystem/index.tsx
@@ -0,0 +1,469 @@
+import { ClipboardDocumentIcon, MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { ValidatedForm } from "remix-validated-form";
+import { Container } from "~/components/Container";
+import { useRef } from "react";
+import { ValidatedCombobox } from "~/components/combobox";
+import { Field, Label } from "~/components/field";
+import { ValidatedInput } from "~/components/input";
+import { Link, useSubmit } from "@remix-run/react";
+import { Checkbox } from "~/components/checkbox";
+import { withZod } from "@remix-validated-form/with-zod";
+import { LaborMarketSearchSchema } from "~/domain";
+import { Button } from "~/components/button";
+import { ValidatedSelect } from "~/components/select";
+import { Header, Row, Table } from "~/components/table";
+import { Badge } from "~/components/Badge";
+import { fromNow } from "~/utils/date";
+import { CopyToClipboard } from "~/components/copy-to-clipboard";
+import { Submission } from "@prisma/client";
+import { findChallenge } from "~/services/challenges-service.server";
+import { typedjson } from "remix-typedjson";
+import { useTypedLoaderData } from "remix-typedjson/dist/remix";
+import { z } from "zod";
+import type { DataFunctionArgs } from "@remix-run/server-runtime";
+import { notFound } from "remix-utils";
+import { countReviews } from "~/services/review-service.server";
+import { f } from "vitest/dist/index-9f5bc072";
+import { Card } from "~/components/Card";
+
+//change
+const validator = withZod(LaborMarketSearchSchema);
+
+export const loader = async ({ params }: DataFunctionArgs) => {
+  //change
+  const id = "3c1985de-4999-499b-82c5-63f5b070c551";
+  const challenge = await findChallenge(id);
+  if (!challenge) {
+    throw notFound({ id });
+  }
+
+  return typedjson({ challenge }, { status: 200 });
+};
+
+export default function Ecosystem() {
+  const { challenge } = useTypedLoaderData<typeof loader>();
+
+  return (
+    <Container className="py-16 px-10">
+      <section className="space-y-2 max-w-3xl mb-16">
+        <h1 className="text-3xl font-semibold">Ecosystem</h1>
+        <div>
+          <p className="text-lg text-sky-500">
+            Discover top submissions, rMETRIC holders, participants, and ecosystem metrics
+          </p>
+          <p className="text-gray-500 text-md">Quickly surface relevant challenge activity and metrics over time</p>
+        </div>
+        <div className="space-x-3 pt-5">
+          <Button size="sm" variant="cancel" disabled>
+            Showcase
+          </Button>
+          <Button size="sm">
+            <Link to="./metrics">Metrics</Link>
+          </Button>
+        </div>
+      </section>
+      <section className="flex flex-col-reverse md:flex-row space-y-reverse gap-y-7 gap-x-5">
+        <main className="flex-1 space-y-10">
+          <div className="space-y-3">
+            <p className="text-lg font-semibold">Top Submissions</p>
+            <hr className="h-1 bg-gray-200" />
+            <SubmissionsListView submissions={challenge.submissions} />
+            <p>{"View {count} more"}</p>
+          </div>
+          <div className="space-y-3">
+            <p className="text-lg font-semibold">Top rMETRIC holders</p>
+            <hr className="h-1 bg-gray-200" />
+            <div className="bg-stone-200 rounded-md w-full h-44 border flex items-center">
+              <p className="py-20 mx-auto">Totally a chart</p>
+            </div>
+            <RMETRICHoldersListView />
+            <p>{"View {count} more"}</p>
+          </div>
+          <div className="space-y-3">
+            <p className="text-lg font-semibold">Top Particpants</p>
+            <hr className="h-1 bg-gray-200" />
+            <div className="flex flex-col md:flex-row gap-5">
+              <div className="bg-stone-200 rounded-md w-full h-44 border flex items-center">
+                <p className="py-20 mx-auto">Totally a chart</p>
+              </div>
+              <div className="bg-stone-200 rounded-md w-full h-44 border flex items-center">
+                <p className="py-20 mx-auto">Totally a chart</p>
+              </div>
+            </div>
+            <ParticipantsListView />
+            <p>{"View {count} more"}</p>
+          </div>
+        </main>
+        <aside className="md:w-1/4 lg:w-1/5 pt-11">
+          <SearchAndFilter />
+        </aside>
+      </section>
+    </Container>
+  );
+}
+function SearchAndFilter() {
+  const submit = useSubmit();
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleChange = () => {
+    if (formRef.current) {
+      submit(formRef.current, { replace: true });
+    }
+  };
+
+  return (
+    <ValidatedForm
+      formRef={formRef}
+      method="get"
+      noValidate
+      validator={validator}
+      onChange={handleChange}
+      className="space-y-3 p-4 border border-gray-300/50 rounded-lg bg-blue-300 bg-opacity-5 text-sm sticky top-0"
+    >
+      <ValidatedInput
+        placeholder="Search address"
+        name="q"
+        size="sm"
+        iconRight={<MagnifyingGlassIcon className="w-5 h-5 text-gray-400" />}
+      />
+
+      <h3 className="font-semibold">Filter</h3>
+      <Checkbox name="flow" label="Analyze" value="analyze" />
+      <Checkbox name="flow" label="Brainstorm" value="brainstorm" />
+
+      <Field>
+        <Label>Chain/Project</Label>
+        <ValidatedCombobox name="project" onChange={handleChange} placeholder="Select option" size="sm" options={[]} />
+      </Field>
+
+      <Field>
+        <Label>Score</Label>
+        <ValidatedCombobox name="score" size="sm" onChange={handleChange} placeholder="Select option" options={[]} />
+      </Field>
+
+      <Field>
+        <Label>Timeframe</Label>
+        <ValidatedSelect name="timeframe" size="sm" onChange={handleChange} placeholder="Select option" options={[]} />
+      </Field>
+
+      <Field>
+        <Label>Marketplace</Label>
+        <ValidatedCombobox
+          name="marketplace"
+          size="sm"
+          onChange={handleChange}
+          placeholder="Select option"
+          options={[]}
+        />
+      </Field>
+    </ValidatedForm>
+  );
+}
+
+function SubmissionsListView({ submissions }: { submissions: Submission[] }) {
+  return (
+    <>
+      {/* Desktop */}
+      <div className="hidden lg:block">
+        <SubmissionsTable submissions={submissions} />
+      </div>
+      {/* Mobile */}
+      <div className="block lg:hidden">
+        <SubmissionsCard submissions={submissions} />
+      </div>
+    </>
+  );
+}
+
+function RMETRICHoldersListView() {
+  return (
+    <>
+      {/* Desktop */}
+      <div className="hidden lg:block">
+        <RMETRICHoldersTable />
+      </div>
+      {/* Mobile */}
+      <div className="block lg:hidden">
+        <RMETRICHoldersCard />
+      </div>
+    </>
+  );
+}
+
+function ParticipantsListView() {
+  return (
+    <>
+      {/* Desktop */}
+      <div className="hidden lg:block">
+        <ParticpantsTable />
+      </div>
+      {/* Mobile */}
+      <div className="block lg:hidden">
+        <ParticipantsCard />
+      </div>
+    </>
+  );
+}
+
+function SubmissionsTable({ submissions }: { submissions: Submission[] }) {
+  const w = { address: "0x364875845389cl1hny43i8" };
+
+  return (
+    <Table>
+      <Header columns={12} className="mb-2 pt-2">
+        <Header.Column span={3}>Title</Header.Column>
+        <Header.Column span={2}>User rMETRIC</Header.Column>
+        <Header.Column span={3}>Challenge</Header.Column>
+        <Header.Column span={2}>Chain/Project</Header.Column>
+        <Header.Column>Submitted</Header.Column>
+      </Header>
+      {submissions.map((s) => {
+        return (
+          <Row asChild columns={12} key={s.id}>
+            <Link to={`/app/brainstorm/c/${s.id}`} className="text-sm text-stone-500">
+              <Row.Column span={3}>
+                <div className="flex flex-wrap gap-1">
+                  {s.title}
+                  <img alt="" src="/img/trophy.svg" width={15} />
+                  <p className="text-neutral-400 font-thin">({s.scoreStatus})</p>
+                </div>
+                <div className="flex flex-row items-center gap-x-2">
+                  <img alt="" src="/img/icons/poly.svg" width={15} />
+                  <CopyToClipboard
+                    className="text-stone-500"
+                    content={w.address.length > 10 ? w.address?.slice(0, 6) + "..." + w.address?.slice(-4) : w.address}
+                    iconRight={<ClipboardDocumentIcon />}
+                  />
+                </div>
+              </Row.Column>
+              <Row.Column span={2}>
+                <Badge>400 rMETRIC</Badge>
+              </Row.Column>
+              <Row.Column span={3}>s.serviceRequest.title</Row.Column>
+              <Row.Column span={2}>
+                {/*<div className="flex items-center gap-2 flex-wrap">
+      {m.projects.map((p) => (
+        <Badge key={p.slug} className="pl-2">
+          <ProjectAvatar project={p} />
+          <span className="mx-1">{p.name}</span>
+        </Badge>
+      ))}
+      </div>*/}
+              </Row.Column>
+              <Row.Column span={2}>{fromNow(s.createdAt)}</Row.Column>
+            </Link>
+          </Row>
+        );
+      })}
+    </Table>
+  );
+}
+
+function SubmissionsCard({ submissions }: { submissions: Submission[] }) {
+  const w = { address: "0x364875845389cl1hny43i8" };
+
+  return (
+    <div className="space-y-4">
+      {submissions.map((s) => {
+        return (
+          <Card asChild key={s.id}>
+            <Link
+              to={`/app/brainstorm/c/${s.id}`}
+              className="text-sm text-stone-500 grid grid-cols-2 gap-y-3 gap-x-1 items-center px-4 py-5"
+            >
+              <div className="col-span-2">
+                <div className="flex gap-1">
+                  {s.title}
+                  <img alt="" src="/img/trophy.svg" width={15} />
+                  <p className="text-neutral-400 font-thin">({s.scoreStatus})</p>
+                </div>
+                <div className="flex flex-row items-center gap-x-2">
+                  <img alt="" src="/img/icons/poly.svg" width={15} />
+                  <CopyToClipboard
+                    className="text-stone-500"
+                    content={w.address.length > 10 ? w.address?.slice(0, 6) + "..." + w.address?.slice(-4) : w.address}
+                    iconRight={<ClipboardDocumentIcon />}
+                  />
+                </div>
+              </div>
+              <p>User rMETRIC</p>
+              <Badge>400 rMETRIC</Badge>
+              <p>Challenge</p>
+              <p>s.serviceRequest.title</p>
+              <p>Chain/Project</p>
+              <div className="flex items-center gap-2 flex-wrap">
+                {/*{m.projects.map((p) => (
+        <Badge key={p.slug} className="pl-2">
+          <ProjectAvatar project={p} />
+          <span className="mx-1">{p.name}</span>
+        </Badge>
+      ))}*/}
+                todo
+              </div>
+              <p>Submitted</p>
+              {fromNow(s.createdAt)}
+            </Link>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
+
+function RMETRICHoldersTable() {
+  return (
+    <Table>
+      <Header columns={6} className="mb-2 pt-2">
+        <Header.Column span={4}>Address</Header.Column>
+        <Header.Column>% of Supply</Header.Column>
+        <Header.Column>Balance</Header.Column>
+      </Header>
+      {[1, 2].map((p) => {
+        return (
+          <Row asChild columns={6} key={p}>
+            <Link to={`/app/u/${p}`} className="text-sm font-medium text-stone-500">
+              <Row.Column span={4} className="flex flex-row items-center gap-x-2">
+                <img alt="" src="/img/icons/poly.svg" />
+                <CopyToClipboard
+                  className="text-stone-500"
+                  content={"0xb794f5ea0ba39494ce839613cccba74279579268"}
+                  iconRight={<ClipboardDocumentIcon className="ml-0.5" />}
+                />
+              </Row.Column>
+              <Row.Column>11.82%</Row.Column>
+              <Row.Column>
+                <Badge>400 rMETRIC</Badge>
+              </Row.Column>
+            </Link>
+          </Row>
+        );
+      })}
+    </Table>
+  );
+}
+
+function RMETRICHoldersCard() {
+  const w = { address: "0xb794f5ea0ba39494ce839613cccba74279579268" };
+
+  return (
+    <div className="space-y-4">
+      {[1, 2, 3].map((p) => {
+        return (
+          <Card key={p} className="px-4 py-5">
+            <Link to={`/app/u/${p}`} className="text-sm font-medium text-stone-500 space-y-3">
+              <div className="flex flex-row items-center gap-x-2 col-span-2">
+                <img alt="" src="/img/icons/poly.svg" />
+                <CopyToClipboard
+                  className="text-stone-500"
+                  content={w.address.length > 10 ? w.address?.slice(0, 7) + "..." + w.address?.slice(-5) : w.address}
+                  iconRight={<ClipboardDocumentIcon className="ml-0.5" />}
+                />
+              </div>
+              <div className="flex flex-wrap items-center justify-between gap-y-2">
+                <div className="flex items-center gap-2">
+                  <p>User rMETRIC</p>
+                  <Badge>400 rMETRIC</Badge>
+                </div>
+                <p>{"% of Supply: {todo}"}</p>
+              </div>
+            </Link>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
+
+function ParticpantsTable() {
+  const w = { address: "0xb794f5ea0ba39494ce839613cccba74279579268" };
+
+  return (
+    <Table>
+      <Header columns={12} className="mb-2 pt-2">
+        <Header.Column span={4}>Address</Header.Column>
+        <Header.Column span={2}>rMETRIC</Header.Column>
+        <Header.Column span={2}>Challenges Launched</Header.Column>
+        <Header.Column span={2}>Challenges Won</Header.Column>
+        <Header.Column>Tokens Funded</Header.Column>
+        <Header.Column>Tokens Claimed</Header.Column>
+      </Header>
+      {[1, 2].map((p) => {
+        return (
+          <Row asChild columns={12} key={p}>
+            <Link to={`/app/brainstorm/c/${p}`} className="text-sm font-medium text-stone-500">
+              <Row.Column span={4} className="flex flex-row items-center gap-x-2">
+                <img alt="" src="/img/icons/poly.svg" />
+                <CopyToClipboard
+                  className="text-stone-500"
+                  content={w.address.length > 10 ? w.address?.slice(0, 6) + "..." + w.address?.slice(-4) : w.address}
+                  iconRight={<ClipboardDocumentIcon className="ml-0.5" />}
+                />
+              </Row.Column>
+              <Row.Column span={2}>
+                <Badge>400 rMETRIC</Badge>
+              </Row.Column>
+              <Row.Column span={2} className="text-center">
+                70
+              </Row.Column>
+              <Row.Column span={2} className="text-center">
+                22
+              </Row.Column>
+              <Row.Column span={2} className="flex gap-1 items-center">
+                <img src="/img/icons/dollar.svg" alt="" />
+                73248
+              </Row.Column>
+              <Row.Column span={2} className="flex gap-1 items-center">
+                <img src="/img/icons/dollar.svg" alt="" />
+                73248
+              </Row.Column>
+            </Link>
+          </Row>
+        );
+      })}
+    </Table>
+  );
+}
+
+function ParticipantsCard() {
+  const w = { address: "0xb794f5ea0ba39494ce839613cccba74279579268" };
+
+  return (
+    <div className="space-y-4">
+      {[1, 2].map((p) => {
+        return (
+          <Card asChild key={p}>
+            <Link
+              to={`/app/u/${p}`}
+              className="text-sm font-medium text-stone-500 grid grid-cols-2 gap-y-3 gap-x-1 items-center px-4 py-5"
+            >
+              <div className="flex flex-row items-center gap-x-2 col-span-2">
+                <img alt="" src="/img/icons/poly.svg" />
+                <CopyToClipboard
+                  className="text-stone-500"
+                  content={w.address.length > 10 ? w.address?.slice(0, 6) + "..." + w.address?.slice(-4) : w.address}
+                  iconRight={<ClipboardDocumentIcon className="ml-0.5" />}
+                />
+              </div>
+              <p>User rMETRIC</p>
+              <Badge>400 rMETRIC</Badge>
+              <p>Challenges Launched</p>
+              <p>70</p>
+              <p>Challenges Won</p>
+              <p>22</p>
+              <p>Tokens Funded</p>
+              <div className="flex gap-1 items-center">
+                <img src="/img/icons/dollar.svg" alt="" />
+                73248
+              </div>
+              <p>Tokens Claimed</p>
+              <div className="flex gap-1 items-center">
+                <img src="/img/icons/dollar.svg" alt="" />
+                73248
+              </div>
+            </Link>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
