e0dc05c7e6db5dd8539679c20d734ffe8e505fe9
Bryan Cinman <bcinman@fastmail.com>
2022-12-08 12:46:28 -0500

    fix: bg blur div overflow

--- app/features/shell/shell.tsx
+++ app/features/shell/shell.tsx
@@ -4,7 +4,7 @@ import { AppHeader } from "./shell-header";
 export function Shell({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative min-h-screen">
-      <div className="absolute w-screen h-screen top-0 left-0 z-0" aria-hidden>
+      <div className="absolute w-screen h-full top-0 left-0 z-0 overflow-x-hidden" aria-hidden>
         <img src="/img/bg-blur.png" className="absolute -top-2/3 left-1/3 opacity-30" alt="bg blur vibes" />
         <img src="/img/bg-blur.png" className="absolute top-1/2 left-1/2 opacity-20" alt="bg blur vibes" />
         <img src="/img/bg-blur.png" className="absolute top-1/5 -left-[600px] opacity-20" alt="bg blur vibes" />
