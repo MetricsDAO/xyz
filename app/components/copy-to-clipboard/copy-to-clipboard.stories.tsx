c6a273055a69bbfc35bdeac9f5597aa0494bc31b
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2022-12-09 17:14:31 -0600

    Make content line up

--- app/components/copy-to-clipboard/copy-to-clipboard.stories.tsx
+++ app/components/copy-to-clipboard/copy-to-clipboard.stories.tsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "./copy-to-clipboard";
 
 export const Basic = () => {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 items-center">
       <CopyToClipboard content="Red text" className="text-red-600" />
       <CopyToClipboard content="This is some content" />
       <CopyToClipboard content="Large text" className="text-lg" />
