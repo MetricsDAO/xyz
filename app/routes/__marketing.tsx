fdf98a6dea1eeb50f63b1abaf8f57c7fffd55761
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2022-12-21 17:01:15 -0600

    Marketing shell imports

--- app/routes/__marketing.tsx
+++ app/routes/__marketing.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "@remix-run/react";
-import Footer from "~/features/marketing-shell/Footer";
-import Header from "~/features/marketing-shell/Header";
+import Footer from "~/features/marketing-shell/footer";
+import Header from "~/features/marketing-shell/header";
 
 // This layout is mostly used to isolate the marketing css stuff from the rest of the app.
 export default function Marketing() {
