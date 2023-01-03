0a22518bb0950e714e98640d006f74494e1400d2
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2023-01-03 17:19:03 -0300

    more cleanup

--- app/hooks/use-create-marketplace.ts
+++ app/hooks/use-create-marketplace.ts
@@ -1,13 +1,6 @@
 import type { TransactionReceipt } from "@ethersproject/abstract-provider";
 import { BigNumber } from "ethers";
-import {
-  LaborMarket,
-  LaborMarketNetwork,
-  LikertEnforcement,
-  PaymentModule,
-  ReputationEngine,
-  ReputationModule,
-} from "labor-markets-abi";
+import { LaborMarket, LaborMarketNetwork, LikertEnforcement, PaymentModule, ReputationModule } from "labor-markets-abi";
 import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
 import type { LaborMarketPrepared } from "~/domain";
 
@@ -32,13 +25,24 @@ export function useCreateMarketplace({
         enforcementModule: LikertEnforcement.address as `0x${string}`,
         paymentModule: PaymentModule.address as `0x${string}`,
         marketUri: data.ipfsHash,
+        // TODO: Uncomment this once we have a way to get the badge address and token id
+        // delegateBadge:
+        //   data.launch.access === "delegates"
+        //     ? (data.launch.badgerAddress as `0x${string}`)
+        //     : (data.reviewBadgerAddress as `0x${string}`), //TODO: reviewers as delegates should not be the "anyone" case
+        // delegateTokenId:
+        //   data.launch.access === "delegates"
+        //     ? BigNumber.from(data.launch.badgerTokenId)
+        //     : BigNumber.from(data.reviewBadgerTokenId), //TODO: reviewers as delegates should not be the "anyone" case
+        // maintainerBadge: data.reviewBadgerAddress as `0x${string}`,
+        // maintainerTokenId: BigNumber.from(data.reviewBadgerTokenId),
         delegateBadge: "0x0d033b4307231711e437937850ebf9ff6bfeeb82",
         delegateTokenId: BigNumber.from(1),
         maintainerBadge: "0x0d033b4307231711e437937850ebf9ff6bfeeb82",
         maintainerTokenId: BigNumber.from(1),
         reputationModule: ReputationModule.address as `0x${string}`,
         reputationConfig: {
-          reputationEngine: "0x305aD87b3eD2132EF1d90dF26d3081511B001650",
+          reputationEngine: "0x305aD87b3eD2132EF1d90dF26d3081511B001650", // TODO: Manually created rep engine. Should come from labor-markets-abi in the future.
           signalStake: BigNumber.from(1), //TODO
           providerThreshold: BigNumber.from(1), //TODO
           maintainerThreshold: BigNumber.from(data.submitRepMin), //TODO
--- app/services/challenges-service.server.ts
+++ app/services/challenges-service.server.ts
@@ -75,7 +75,7 @@ export const prepareChallenge = (newChallenge: ChallengeNew): ChallengePrepared 
     laborMarketAddress: "0xf48cdadfa609f0348d9e5c14f2801be0a45e0a33", // recently created labor market on Goerli https://goerli.etherscan.io/address/0xf48cdadfa609f0348d9e5c14f2801be0a45e0a33
     pTokenAddress: "0xBA62BCfcAaFc6622853cca2BE6Ac7d845BC0f2Dc", // FAU token https://erc20faucet.com/
     pTokenQuantity: 0, //TODO
-    pTokenId: 0, // TODO
+    pTokenId: 0, // Not used by contract. Left over appendage from when we were using ERC1155
     uri: "ipfs-uri",
     enforcementExpiration: customStringToDate(newChallenge.reviewEndDate, newChallenge.reviewEndTime),
     submissionExpiration: customStringToDate(newChallenge.endDate, newChallenge.endTime),
