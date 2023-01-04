43fe756ee4e2d6fbf7a7bb172d97b9031dd2815f
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2023-01-04 15:34:36 -0600

    Basic page

--- app/routes/__marketing.tsx
+++ app/routes/__marketing.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from "@remix-run/react";
-import Footer from "~/features/marketing-shell/footer";
 import Header from "~/features/marketing-shell/header";
 
 // This layout is mostly used to isolate the marketing css stuff from the rest of the app.
