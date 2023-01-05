7767be1d028d368efce2d46e749058b053eefbfb
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2023-01-05 16:36:35 -0600

    Make it work for 300 wide

--- app/routes/__marketing/partner.tsx
+++ app/routes/__marketing/partner.tsx
@@ -76,11 +76,13 @@ export default function Index() {
                   <div className="absolute top-0 left-0 text-white text-base max-w-xs m-5 space-y-5">
                     <img src="/img/marketing/partner-rocket.png" alt="" className="hidden sm:block" />
                     <div className="space-y-1">
-                      <p className="text-2xl font-bold">Analytics on Demand</p>
-                      <p className="text-xl">Starts at $1,000</p>
+                      <p className="text-xl sm:text-2xl font-bold">Analytics on Demand</p>
+                      <p className="text-lg sm:text-xl">Starts at $1,000</p>
                     </div>
-                    <p>Access on-demand blockchain analytics from our community of experts.</p>
-                    <p>For any organization or project in need of Web3 analytics.</p>
+                    <p className="text-xs sm:text-base">
+                      Access on-demand blockchain analytics from our community of experts.
+                    </p>
+                    <p className="text-xs sm:text-base">For any organization or project in need of Web3 analytics.</p>
                   </div>
                 </div>
                 <div className="relative mx-auto">
@@ -88,13 +90,13 @@ export default function Index() {
                   <div className="absolute top-0 left-0 text-white text-base max-w-xs m-5 space-y-5">
                     <img src="/img/marketing/partner-plant.png" alt="" className="hidden sm:block" />
                     <div className="space-y-1">
-                      <p className="text-2xl font-bold">Community Growth</p>
-                      <p className="text-xl">Starts at $10,000</p>
+                      <p className="text-xl sm:text-2xl  font-bold">Community Growth</p>
+                      <p className="text-lg sm:text-xl">Starts at $10,000</p>
                     </div>
-                    <p>
+                    <p className="text-xs sm:text-base">
                       Onboard and grow a community of analysts to drive adoption, usage, and analytics for your project.
                     </p>
-                    <p>For any blockchain or protocol with curated data.</p>
+                    <p className="text-xs sm:text-base">For any blockchain or protocol with curated data.</p>
                   </div>
                 </div>
                 <div className="relative mx-auto">
@@ -102,11 +104,13 @@ export default function Index() {
                   <div className="absolute top-0 left-0 text-white text-base max-w-xs m-5 space-y-5">
                     <img src="/img/marketing/partner-butterfly.png" alt="" className="hidden sm:block" />
                     <div className="space-y-1">
-                      <p className="text-2xl font-bold">Data Readiness</p>
-                      <p className="text-xl">Starts at $20,000</p>
+                      <p className="text-xl sm:text-2xl font-bold">Data Readiness</p>
+                      <p className="text-lg sm:text-xl">Starts at $20,000</p>
                     </div>
-                    <p>Unlock potential adoption, usage, and analytics for your project with community curated data.</p>
-                    <p>For any blockchain or protocol in need of data curation.</p>
+                    <p className="text-xs sm:text-base">
+                      Unlock potential adoption, usage, and analytics for your project with community curated data.
+                    </p>
+                    <p className="text-xs sm:text-base">For any blockchain or protocol in need of data curation.</p>
                   </div>
                 </div>
               </div>
