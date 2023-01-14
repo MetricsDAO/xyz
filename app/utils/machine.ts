6a36320179b96946fc6f9d98fb686d31df7f624e
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2023-01-14 10:55:18 -0300

    fix machine

--- app/utils/machine.ts
+++ app/utils/machine.ts
@@ -1,63 +1,71 @@
 import { assign, createMachine } from "xstate";
 import type { LaborMarketContract } from "~/domain";
 
-export const blockchainMachine = createMachine({
-  id: "chain-write",
-  initial: "idle",
-  context: {
-    contractData: undefined,
-  },
-  schema: {
-    context: {} as { contractData?: LaborMarketContract },
-    events: {} as
-      | { type: "TRANSACTION_PREPARE" }
-      | { type: "TRANSACTION_READY"; data: LaborMarketContract }
-      | { type: "TRANSACTION_CANCEL" }
-      | { type: "TRANSACTION_WRITE" }
-      | { type: "TRANSACTION_SUCCESS" },
-  },
-  predictableActionArguments: true,
-  states: {
-    idle: {
-      on: {
-        TRANSACTION_PREPARE: { target: "transactionPrepare" },
-      },
+export const blockchainMachine = createMachine(
+  {
+    id: "chain-write",
+    initial: "idle",
+    context: {
+      contractData: undefined,
     },
-    transactionPrepare: {
-      initial: "loading",
-      on: {
-        TRANSACTION_READY: {
-          target: "transactionReady",
-          actions: assign({
-            contractData: (context, event) => {
-              if (event.type === "TRANSACTION_READY") {
-                return event.data;
-              }
-            },
-          }),
+    schema: {
+      context: {} as { contractData?: LaborMarketContract },
+      events: {} as
+        | { type: "TRANSACTION_PREPARE" }
+        | { type: "TRANSACTION_READY"; data: LaborMarketContract }
+        | { type: "TRANSACTION_CANCEL" }
+        | { type: "TRANSACTION_WRITE" }
+        | { type: "TRANSACTION_SUCCESS" },
+    },
+    predictableActionArguments: true,
+    states: {
+      idle: {
+        on: {
+          TRANSACTION_PREPARE: { target: "transactionPrepare" },
         },
       },
-      states: {
-        loading: {},
-        failure: {
-          entry: "notifyTransactionPrepareFailure",
+      transactionPrepare: {
+        initial: "loading",
+        on: {
+          TRANSACTION_READY: {
+            target: "transactionReady",
+            actions: "setContractData",
+          },
+        },
+        states: {
+          loading: {},
+          failure: {
+            entry: "notifyTransactionPrepareFailure",
+          },
         },
       },
-    },
-    transactionReady: {
-      on: {
-        TRANSACTION_CANCEL: { target: "idle" }, //start over
-        TRANSACTION_WRITE: { target: "transactionWrite" },
+      transactionReady: {
+        on: {
+          TRANSACTION_CANCEL: { target: "idle" }, //start over
+          TRANSACTION_WRITE: { target: "transactionWrite" },
+        },
       },
-    },
-    transactionWrite: {
-      entry: "notifyTransactionWrite",
-      on: {
-        TRANSACTION_SUCCESS: { target: "transactionComplete" },
+      transactionWrite: {
+        entry: "notifyTransactionWrite",
+        on: {
+          TRANSACTION_SUCCESS: { target: "transactionComplete" },
+        },
       },
-    },
-    transactionComplete: {
-      entry: "notifyTransactionSuccess",
+      transactionComplete: {
+        entry: "notifyTransactionSuccess",
+      },
     },
   },
-});
+  {
+    actions: {
+      setContractData: assign({
+        contractData: (context, event) => {
+          if (event.type === "TRANSACTION_READY") {
+            return event.data;
+          }
+          return context.contractData;
+        },
+      }),
+    },
+  }
+);
