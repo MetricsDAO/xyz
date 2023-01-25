ba02478e03b803bbe570c15e473a02a7193f17e1
Hannah Lerman <43100701+thehannahlerman@users.noreply.github.com>
2023-01-25 14:47:31 -0500

    save

--- app/routes/app/$mType/m/$laborMarketAddress.sr/$serviceRequestId.claim.tsx
+++ app/routes/app/$mType/m/$laborMarketAddress.sr/$serviceRequestId.claim.tsx
@@ -1,5 +1,5 @@
-import type { DataFunctionArgs } from "@remix-run/server-runtime";
-import { typedjson, useTypedLoaderData } from "remix-typedjson";
+import type { ActionArgs, DataFunctionArgs } from "@remix-run/server-runtime";
+import { typedjson, useTypedActionData, useTypedLoaderData } from "remix-typedjson";
 import { notFound } from "remix-utils";
 import { z } from "zod";
 import { findServiceRequest } from "~/services/service-request.server";
@@ -8,12 +8,22 @@ import { CountdownCard } from "~/components/countdown-card";
 import { Button } from "~/components/button";
 import { Badge } from "~/components/badge";
 import type { ClaimToSubmitPrepared } from "~/domain";
-import { useState } from "react";
+import { ClaimToSubmitContractSchema } from "~/domain";
+import { useEffect, useState } from "react";
 import invariant from "tiny-invariant";
 import { useClaimToSubmit } from "~/hooks/use-claim-to-submit";
 import toast from "react-hot-toast";
 import { Modal } from "~/components";
 import { useParams } from "@remix-run/react";
+import { ClaimToSubmitWeb3Button } from "~/features/web3-button/claim-to-submit";
+import { useMachine } from "@xstate/react";
+import { defaultNotifyTransactionActions } from "~/features/web3-transaction-toasts";
+import { createBlockchainTransactionStateMachine } from "~/utils/machine";
+import type { ValidationErrorResponseData } from "remix-validated-form";
+import { validationError } from "remix-validated-form";
+import { getUser } from "~/services/session.server";
+import { withZod } from "@remix-validated-form/with-zod";
+import { isValidationError } from "~/utils/utils";
 
 const paramsSchema = z.object({ laborMarketAddress: z.string(), serviceRequestId: z.string() });
 export const loader = async ({ params }: DataFunctionArgs) => {
@@ -26,15 +36,48 @@ export const loader = async ({ params }: DataFunctionArgs) => {
   return typedjson({ serviceRequest }, { status: 200 });
 };
 
+const validator = withZod(ClaimToSubmitContractSchema);
+
+type ActionResponse = { claim: ClaimToSubmitPrepared } | ValidationErrorResponseData;
+export const action = async ({ request }: ActionArgs) => {
+  const user = await getUser(request);
+  invariant(user, "You must be logged in to claim a submission");
+  const result = await validator.validate(await request.formData());
+  if (result.error) return validationError(result.error);
+
+  return typedjson({ claim: result.data });
+};
+
 export default function ClaimToSubmit() {
+  const actionData = useTypedActionData<ActionResponse>();
+
   const { serviceRequest } = useTypedLoaderData<typeof loader>();
   const { mType } = useParams();
 
   const [modalData, setModalData] = useState<{ data?: ClaimToSubmitPrepared; isOpen: boolean }>({ isOpen: false });
 
+  const claimToSubmitMachine = createBlockchainTransactionStateMachine<ClaimToSubmitPrepared>();
+
   function closeModal() {
     setModalData((previousInputs) => ({ ...previousInputs, isOpen: false }));
   }
+
+  const [state, send] = useMachine(claimToSubmitMachine, {
+    actions: {
+      notifyTransactionWait: (context) => {
+        // Link to transaction? https://goerli.etherscan.io/address/${context.transactionHash}
+        toast.loading("Creating marketplace...", { id: "creating-marketplace" });
+      },
+      notifyTransactionSuccess: () => {
+        toast.dismiss("creating-marketplace");
+        toast.success("Marketplace created!");
+      },
+      notifyTransactionFailure: () => {
+        toast.dismiss("creating-marketplace");
+        toast.error("Marketplace creation failed");
+      },
+    },
+  });
 
   return (
     <Container className="max-w-4xl space-y-7 py-16">
@@ -99,42 +142,19 @@ export default function ClaimToSubmit() {
         <Button variant="cancel">Cancel</Button>
       </div>
       <div className="invisible"></div>
-      <Modal title="Claim to submit?" isOpen={modalData.isOpen} onClose={closeModal}>
-        <ConfirmTransaction data={modalData.data} onClose={closeModal} />
-      </Modal>
+      {state.context.contractData && (
+        <Modal title="Create Marketplace?" isOpen={isModalOpen} onClose={closeModal}>
+          <div className="space-y-8">
+            <p>Please confirm that you would like to create a new marketplace.</p>
+            <div className="flex flex-col sm:flex-row justify-center gap-5">
+              <ClaimToSubmitWeb3Button data={state.context.contractData} onWriteSuccess={onWriteSuccess} />
+              <Button variant="cancel" size="md" onClick={closeModal}>
+                Cancel
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      )}
     </Container>
-  );
-}
-
-function ConfirmTransaction({ data, onClose }: { data?: ClaimToSubmitPrepared; onClose: () => void }) {
-  invariant(data, "ClaimToSubmitPrepared is required"); // this should never happen but just in case
-
-  const { write, isLoading } = useClaimToSubmit({
-    data: data,
-    onTransactionSuccess() {
-      toast.dismiss("claiming-to-submit");
-      toast.success("Challenge Claimed!");
-    },
-    onWriteSuccess() {
-      toast.loading("Claiming challenge to submit...", { id: "claiming-to-submit" });
-    },
-  });
-
-  const onCreate = () => {
-    write?.();
-  };
-
-  return (
-    <div className="space-y-8 mt-4">
-      <p>Are you sure you want to claim a submission for this challenge?</p>
-      <div className="flex flex-wrap gap-5">
-        <Button loading={isLoading} onClick={onCreate}>
-          Claim
-        </Button>
-        <Button variant="cancel" onClick={onClose}>
-          Cancel
-        </Button>
-      </div>
-    </div>
   );
 }
