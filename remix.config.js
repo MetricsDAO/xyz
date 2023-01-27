245bf5aeda2ef8c9a557720dfff0c9edc04df26a
Bryan Cinman <bcinman@fastmail.com>
2023-01-27 00:02:18 -0500

    whitepaper

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverDependenciesToBundle: ["@rainbow-me/rainbowkit", "@rainbow-me/rainbowkit/wallets"],
+  serverDependenciesToBundle: ["@rainbow-me/rainbowkit", "@rainbow-me/rainbowkit/wallets", "mdx-bundler/client"],
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
   // appDirectory: "app",
