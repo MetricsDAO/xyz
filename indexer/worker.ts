a7388afc66f69760fd2b369624325e77db05a6d9
Hannah Lerman <43100701+thehannahlerman@users.noreply.github.com>
2023-02-02 15:55:00 -0500

    oops

--- indexer/worker.ts
+++ indexer/worker.ts
@@ -11,7 +11,7 @@ const worker = pine.createWorker({
   logger: logger,
   tracer: {
     namespace: env.PINE_NAMESPACE,
-    version: "0.0.2",
+    version: "1.4.2-2",
     blockchain: { name: "polygon", network: "mainnet" },
   },
 });
