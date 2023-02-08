1f7e378deb29d71c17e951ec81d01a397ee2a69d
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2023-02-08 17:31:58 -0500

    test foundation

---
+++ app/utils/date.test.ts
@@ -0,0 +1,9 @@
+import { progressTime, countDown } from "./date";
+
+describe("progressTime", async () => {
+  test("simple flow", async () => {});
+});
+
+describe("countDown", async () => {
+  test("simple flow", async () => {});
+});
