8974baf1d0cfadf04717d552c65c03dbc228ba6e
termaatFlipside <morgan.termaat@flipsidecrypto.com>
2023-02-09 10:53:11 -0500

    small thoughts

--- app/utils/date.test.ts
+++ app/utils/date.test.ts
@@ -1,9 +1,14 @@
 import { progressTime, countDown } from "./date";
 
 describe("progressTime", async () => {
-  test("simple flow", async () => {});
+  test("Over 100", async () => {});
+  test("Around 50", async () => {});
+  test("Hasn't started yet", async () => {});
 });
 
 describe("countDown", async () => {
-  test("simple flow", async () => {});
+  test("Month leading", async () => {});
+  test("Day leading", async () => {});
+  test("Hour leading", async () => {}); //might be hard to be exact w/seconds
+  test("Day has passed", async () => {});
 });
