bd2c4c816c3125263b3e33e71dacc61cc8733d4a
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2023-02-10 10:29:16 -0300

    slightly better UI

--- app/features/rpc-error.tsx
+++ app/features/rpc-error.tsx
@@ -1,5 +1,10 @@
 import type { EthersError } from "./web3-button/types";
 
 export function RPCError({ error }: { error: EthersError }) {
-  return <p className="text-red-500">{error.reason ?? "Something went wrong"}</p>;
+  return (
+    <div className="space-y-3">
+      <p className="text-red-500 font-semibold">Something went wrong in preparing your transaction</p>
+      <p className="text-red-500">{error.reason}</p>
+    </div>
+  );
 }
