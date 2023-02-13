7fb4384e6fb4a1c8e3d7090cde5dfd42ba325db4
Hannah Lerman <43100701+thehannahlerman@users.noreply.github.com>
2023-02-13 15:56:23 -0500

    fix avatar issue

--- app/features/connect-button.tsx
+++ app/features/connect-button.tsx
@@ -41,7 +41,7 @@ function CustomConnectButton() {
             <Button onClick={openAccountModal} variant="gradient">
               <div className="flex flex-row gap-2 items-center align-middle">
                 {account.ensAvatar ? (
-                  account.ensAvatar
+                  <img src={account.ensAvatar} className="rounded-full w-8" alt="" />
                 ) : (
                   <div className="rounded-full">
                     <Jazzicon diameter={20} seed={jsNumberForAddress(account.address)} />
