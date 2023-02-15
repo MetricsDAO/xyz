7af5d04ad29a16d960afbb47980e479cccc98505
termaatFlipside <morgan.termaat@flipsidecrypto.com>
2023-02-15 10:15:52 -0500

    wip

--- app/features/project-badges.tsx
+++ app/features/project-badges.tsx
@@ -1,10 +1,28 @@
 import type { Project } from "@prisma/client";
+import { useState } from "react";
 import { Badge, ProjectAvatar } from "~/components";
 
-export function ProjectBadges({ projects }: { projects: Project[] }) {
+export function ProjectBadges({ projects, max }: { projects: Project[]; max: number }) {
+  const [allIcons, setAllIcons] = useState<boolean>(false);
+  if (projects.length <= max) {
+    return (
+      <div className="flex flex-wrap gap-2">
+        {projects.map((p) => {
+          return (
+            <Badge key={p.id} className="pl-2">
+              <ProjectAvatar project={p} />
+              <span className="mx-1">{p.name}</span>
+            </Badge>
+          );
+        })}
+      </div>
+    );
+  }
+
+  const surplus = projects.length - max;
   return (
     <div className="flex flex-wrap gap-2">
-      {projects.map((p) => {
+      {projects.slice(0, max).map((p) => {
         return (
           <Badge key={p.id} className="pl-2">
             <ProjectAvatar project={p} />
@@ -12,6 +30,22 @@ export function ProjectBadges({ projects }: { projects: Project[] }) {
           </Badge>
         );
       })}
+      <div onClick={() => setAllIcons(!allIcons)} className="flex flex-wrap gap-2">
+        {allIcons ? (
+          <>
+            {projects.slice(max).map((p) => {
+              return (
+                <Badge key={p.id} className="pl-2">
+                  <ProjectAvatar project={p} />
+                  <span className="mx-1">{p.name}</span>
+                </Badge>
+              );
+            })}
+          </>
+        ) : (
+          <Badge>{`${surplus}+`}</Badge>
+        )}
+      </div>
     </div>
   );
 }
