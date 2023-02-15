a1811528932ea40b865e228154c5249f91068484
termaatFlipside <108745325+termaatFlipside@users.noreply.github.com>
2023-02-15 16:29:15 -0500

    spacing

--- app/components/segmented-radio/segmented-radio.tsx
+++ app/components/segmented-radio/segmented-radio.tsx
@@ -22,9 +22,7 @@ export function SegmentedRadio({
             key={option.value}
             value={option.value}
             className={({ checked }) =>
-              clsx(
-                checked ? "bg-[#EDEDED]" : "bg-white", "border-2 rounded-md py-3 px-10 cursor-pointer"
-              )
+              clsx(checked ? "bg-[#EDEDED]" : "bg-white", "border-2 rounded-md py-3 px-10 cursor-pointer")
             }
           >
             {option.label}
