087552eb87fce558f8b189d903d1d670b8a7235d
termaatFlipside <morgan.termaat@flipsidecrypto.com>
2023-02-20 11:44:18 -0500

    cleanup

--- app/services/submissions.server.ts
+++ app/services/submissions.server.ts
@@ -137,48 +137,45 @@ export const prepareSubmission = async (
  * Returns an array of Submissions with their Reviews for a given Service Request.
  */
 export const searchSubmissionsWithReviews = async (params: SubmissionSearch) => {
-  return (
-    mongo.submissions
-      .aggregate([
-        {
-          $match: {
-            $and: [
-              params.laborMarketAddress ? { laborMarketAddress: params.laborMarketAddress } : {},
-              params.serviceRequestId ? { serviceRequestId: params.serviceRequestId } : {},
-              params.serviceProvider ? { "configuration.serviceProvider": params.serviceProvider } : {},
-              //params.q ? { $text: { $search: params.q, $language: "english" } } : {},
-            ],
-          },
+  return mongo.submissions
+    .aggregate([
+      {
+        $match: {
+          $and: [
+            params.laborMarketAddress ? { laborMarketAddress: params.laborMarketAddress } : {},
+            params.serviceRequestId ? { serviceRequestId: params.serviceRequestId } : {},
+            params.serviceProvider ? { "configuration.serviceProvider": params.serviceProvider } : {},
+            //params.q ? { $text: { $search: params.q, $language: "english" } } : {},
+          ],
         },
-        {
-          $lookup: {
-            from: "reviews",
-            let: {
-              sr_id: "$serviceRequestId",
-              m_addr: "$laborMarketAddress",
-              s_id: "$id",
-            },
-            pipeline: [
-              {
-                $match: {
-                  $expr: {
-                    $and: [
-                      { $eq: ["$submissionId", "$$s_id"] },
-                      { $eq: ["$id", "$$sr_id"] },
-                      { $eq: ["$laborMarketAddress", "$$m_addr"] },
-                    ],
-                  },
+      },
+      {
+        $lookup: {
+          from: "reviews",
+          let: {
+            sr_id: "$serviceRequestId",
+            m_addr: "$laborMarketAddress",
+            s_id: "$id",
+          },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ["$submissionId", "$$s_id"] },
+                    { $eq: ["$id", "$$sr_id"] },
+                    { $eq: ["$laborMarketAddress", "$$m_addr"] },
+                  ],
                 },
               },
-            ],
-            as: "reviews",
-          },
+            },
+          ],
+          as: "reviews",
         },
-      ])
-      //.find(searchParams(params))
-      .sort({ [params.sortBy]: params.order })
-      .skip(params.first * (params.page - 1))
-      .limit(params.first)
-      .toArray()
-  );
+      },
+    ])
+    .sort({ [params.sortBy]: params.order })
+    .skip(params.first * (params.page - 1))
+    .limit(params.first)
+    .toArray();
 };
