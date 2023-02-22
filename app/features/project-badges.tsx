7199f5910c5f963d0f73b004e57b5ee9f9fabaf7
termaatFlipside <morgan.termaat@flipsidecrypto.com>
2023-02-21 23:12:17 -0500

    Display on hover not click

--- app/features/project-badges.tsx
+++ app/features/project-badges.tsx
@@ -1,11 +1,9 @@
 import type { Project } from "@prisma/client";
-import { useState } from "react";
 import { Badge, ProjectAvatar } from "~/components";
 
 const MAX = 3;
 
 export function ProjectBadges({ projects }: { projects: Project[] }) {
-  const [allIcons, setAllIcons] = useState<boolean>(false);
   const surplus = projects.length - MAX;
   return (
     <div className="flex flex-wrap gap-2">
@@ -18,21 +16,18 @@ export function ProjectBadges({ projects }: { projects: Project[] }) {
         );
       })}
       {surplus > 0 && (
-        <div onClick={() => setAllIcons(!allIcons)} className="flex flex-wrap gap-2 cursor-pointer">
-          {allIcons ? (
-            <>
-              {projects.slice(MAX).map((p) => {
-                return (
-                  <Badge key={p.id} className="pl-2">
-                    <ProjectAvatar project={p} />
-                    <span className="mx-1">{p.name}</span>
-                  </Badge>
-                );
-              })}
-            </>
-          ) : (
-            <Badge>{`${surplus}+`}</Badge>
-          )}
+        <div className="flex flex-wrap gap-2 group">
+          <div className="hidden group-hover:flex flex-wrap gap-2">
+            {projects.slice(MAX).map((p) => {
+              return (
+                <Badge key={p.id} className="pl-2">
+                  <ProjectAvatar project={p} />
+                  <span className="mx-1">{p.name}</span>
+                </Badge>
+              );
+            })}
+          </div>
+          <Badge className="block group-hover:hidden">{`${surplus}+`}</Badge>
         </div>
       )}
     </div>
