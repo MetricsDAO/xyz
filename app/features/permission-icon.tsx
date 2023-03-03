de681977ac5d6618df5ff0924eea080ab6ed774d
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2023-03-03 12:11:01 -0300

    simplify permission icon

--- app/features/permission-icon.tsx
+++ app/features/permission-icon.tsx
@@ -1,19 +1,13 @@
 import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/20/solid";
-import clsx from "clsx";
 
 export function PermissionIcon({ isAllowed }: { isAllowed?: boolean }) {
-  return (
-    <>
-      <CheckCircleIcon
-        className={clsx("w-5 h-5 text-lime-600", {
-          hidden: isAllowed === undefined || isAllowed === false,
-        })}
-      />
-      <XCircleIcon
-        className={clsx("w-5 h-5 text-rose-500", {
-          hidden: isAllowed === undefined || isAllowed === true,
-        })}
-      />
-    </>
-  );
+  if (isAllowed === undefined) {
+    return null;
+  }
+
+  if (isAllowed) {
+    return <CheckCircleIcon className="w-5 h-5 text-lime-600" />;
+  }
+
+  return <XCircleIcon className="w-5 h-5 text-rose-500" />;
 }
