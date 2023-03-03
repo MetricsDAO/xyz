ee5c58be68e20ec2d38620f0b8c182ebc1466228
termaatFlipside <morgan.termaat@flipsidecrypto.com>
2023-03-02 23:07:03 -0500

    Hover and max on challenge pool badges

--- app/features/challenge-pool-badges.tsx
+++ app/features/challenge-pool-badges.tsx
@@ -3,6 +3,8 @@ import { Badge, TokenAvatar } from "~/components";
 import type { LaborMarketDoc } from "~/domain";
 import { fromTokenAmount } from "~/utils/helpers";
 
+const MAX = 3;
+
 export function ChallengePoolBadges({
   pools,
   tokens,
@@ -10,9 +12,10 @@ export function ChallengePoolBadges({
   pools: LaborMarketDoc["serviceRequestRewardPools"];
   tokens: Token[];
 }) {
+  const surplus = pools.length - MAX;
   return (
-    <>
-      {pools?.map((p) => {
+    <div className="flex flex-wrap gap-2">
+      {pools.slice(0, MAX).map((p) => {
         const token = tokens.find((t) => t.contractAddress === p.pToken);
         if (token) {
           return (
@@ -26,6 +29,26 @@ export function ChallengePoolBadges({
         }
         return null;
       })}
-    </>
+      {surplus > 0 && (
+        <div className="flex flex-wrap gap-2 group">
+          <div className="hidden group-hover:flex flex-wrap gap-2">
+            {pools.slice(MAX).map((p) => {
+              const token = tokens.find((t) => t.contractAddress === p.pToken);
+              if (token) {
+                return (
+                  <Badge key={token?.id}>
+                    <TokenAvatar token={token} />
+                    <span className="mx-1">
+                      {fromTokenAmount(p.pTokenQuantity)} {token.symbol}
+                    </span>
+                  </Badge>
+                );
+              }
+            })}
+          </div>
+          <Badge className="block group-hover:hidden">{`${surplus}+`}</Badge>
+        </div>
+      )}
+    </div>
   );
 }
