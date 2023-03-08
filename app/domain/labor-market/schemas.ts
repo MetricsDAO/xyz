93a072e8308a0ccd167f041a13ec3b5f8afb436c
Hannah Lerman <43100701+thehannahlerman@users.noreply.github.com>
2023-03-08 15:58:30 -0500

    remove signalStake

--- app/domain/labor-market/schemas.ts
+++ app/domain/labor-market/schemas.ts
@@ -21,7 +21,6 @@ export const LaborMarketModules = z.object({
 
 export const LaborMarketReputationParams = z.object({
   rewardPool: z.coerce.string(),
-  signalStake: z.coerce.string(),
   provideStake: z.coerce.string(),
   submitMin: z.coerce.string(),
   submitMax: z.coerce.string(),
