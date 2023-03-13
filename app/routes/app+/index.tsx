03a1cbfb295ec109f76872d743d6a049432340fe
termaatFlipside <morgan.termaat@flipsidecrypto.com>
2023-03-13 14:23:43 -0400

    Use the correct thing

--- app/routes/app+/index.tsx
+++ app/routes/app+/index.tsx
@@ -1,7 +1,9 @@
+import { redirect } from "@remix-run/node";
+
+export const loader = async () => {
+  return redirect("/app/analyze");
+};
+
 export default function Index() {
-  return (
-    <>
-      <meta http-equiv="refresh" content="0;url=http://metricsdao.xyz/app/analyze" />
-    </>
-  );
+  return <></>;
 }
