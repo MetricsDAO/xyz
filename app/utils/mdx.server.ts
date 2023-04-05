6cdfcbaa8e0e4d69c43c0f734c1a3705c8dc6eaa
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2023-04-05 16:20:31 -0400

    ok this works

--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -1,17 +1,10 @@
 import fs from "fs/promises";
 import { bundleMDX } from "mdx-bundler";
 
-export async function getContent(filename: string) {
-  const p = __dirname;
-  console.log("p", p);
-  const x = await fs.readdir(p);
-  const y = await fs.readdir(p + "..");
-  const z = await fs.readdir(p + "../..");
-  console.log("testing", x, y, z);
+export function getContent(filename: string) {
   return fs.readFile(filename, "utf-8");
 }
 
-export async function getMDXContent(filename: string) {
-  const mdx = await getContent("content/" + filename);
-  return bundleMDX({ source: mdx });
+export async function getMDXContent(source: string) {
+  return bundleMDX({ source });
 }
