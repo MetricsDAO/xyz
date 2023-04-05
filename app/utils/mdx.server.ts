d9f67cc3f4097c4914794528e29dd403ec007c4a
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2023-04-05 14:21:31 -0400

    testing

--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -2,9 +2,12 @@ import fs from "fs/promises";
 import { bundleMDX } from "mdx-bundler";
 
 export async function getContent(filename: string) {
-  const x = await fs.readdir(".");
-  const y = await fs.readdir("build");
-  console.log("testing", x, y);
+  const p = __dirname;
+  console.log("p", p);
+  const x = await fs.readdir(p);
+  const y = await fs.readdir(p + "..");
+  const z = await fs.readdir(p + "../..");
+  console.log("testing", x, y, z);
   return fs.readFile(filename, "utf-8");
 }
 
