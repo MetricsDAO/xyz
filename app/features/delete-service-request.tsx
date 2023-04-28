0c825a37a1df195fc269f381f2f9f7b6e99a9490
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2023-04-28 13:12:44 -0400

    Don't use TxModal

--- app/features/delete-service-request.tsx
+++ app/features/delete-service-request.tsx
@@ -8,37 +8,11 @@ import { ServiceRequestWithIndexData } from "~/domain/service-request/schemas";
 import { useContracts } from "~/hooks/use-root-data";
 import { configureWrite, useTransactor } from "~/hooks/use-transactor";
 import ConnectWalletWrapper from "./connect-wallet-wrapper";
-import { TxModal } from "~/components/tx-modal/tx-modal";
 
 export default function DeleteServiceRequestModal({ serviceRequest }: { serviceRequest: ServiceRequestWithIndexData }) {
-  const [open, setOpen] = useState<boolean>(false);
-  return (
-    <>
-      <div className="border border-neutral-200 p-2.5 rounded cursor-pointer" onClick={() => setOpen(true)}>
-        <TrashIcon className="text-gray-500 h-6 w-6" />
-      </div>
-      <Modal isOpen={open} onClose={() => setOpen(false)} closeButton={false}>
-        <div>
-          <ExclamationTriangleIcon className="h-8 w-8 text-red-500 mx-auto my-4" />
-          <h3 className="text-base text-center font-medium">Are you sure you want to delete this Challenge?</h3>
-          <p className="text-center text-sm text-stone-500 mb-6 mt-2">
-            All information will be removed and you will not be able to retrieve it.
-          </p>
-          <div className="flex gap-2">
-            <Button variant="cancel" fullWidth={true}>
-              Cancel
-            </Button>
-            <ServiceRequestDelete serviceRequest={serviceRequest} />
-          </div>
-        </div>
-      </Modal>
-    </>
-  );
-}
-
-function ServiceRequestDelete({ serviceRequest }: { serviceRequest: ServiceRequestWithIndexData }) {
   const contracts = useContracts();
   const navigate = useNavigate();
+  const [open, setOpen] = useState<boolean>(false);
 
   const transactor = useTransactor({
     onSuccess: useCallback(
@@ -50,7 +24,7 @@ function ServiceRequestDelete({ serviceRequest }: { serviceRequest: ServiceReque
   });
 
   const onClick = () => {
-    console.log("hit");
+    setOpen(true);
     transactor.start({
       config: () => configureFromValues({ contracts, inputs: { serviceRequest } }),
     });
@@ -59,17 +33,55 @@ function ServiceRequestDelete({ serviceRequest }: { serviceRequest: ServiceReque
   return (
     <>
       <ConnectWalletWrapper onClick={onClick}>
-        <Button variant="danger" fullWidth={true}>
-          Delete
-        </Button>
+        <div className="border border-neutral-200 p-2.5 rounded cursor-pointer">
+          <TrashIcon className="text-gray-500 h-6 w-6" />
+        </div>
       </ConnectWalletWrapper>
-      <TxModal
-        transactor={transactor}
-        title="Delete Challenge"
-        confirmationMessage={
-          <p className="mt-2">Please confirm that you would like to delete {serviceRequest.appData.title}</p>
-        }
-      />
+      <Modal isOpen={transactor.state != "idle"} onClose={transactor.cancel}>
+        <div>
+          <ExclamationTriangleIcon className="h-8 w-8 text-red-500 mx-auto my-4" />
+          {transactor.state === "preparing" ? <div className="mx-auto">Preparing...</div> : null}
+
+          {transactor.state === "failure" ? (
+            <div className="text-rose-500 space-y-3">
+              <p>Something went wrong</p>
+              <p>{transactor.error}</p>
+            </div>
+          ) : null}
+          {transactor.state === "prepared" ? (
+            <>
+              <h3 className="text-base text-center font-medium">Are you sure you want to delete this Challenge?</h3>
+              <p className="text-center text-sm text-stone-500 mb-6 mt-2">
+                All information will be removed and you will not be able to retrieve it.
+              </p>{" "}
+              <div className="flex gap-2">
+                <Button variant="cancel" fullWidth={true} onClick={transactor.cancel}>
+                  Cancel
+                </Button>
+                <Button variant="danger" fullWidth={true} onClick={transactor.write}>
+                  Delete
+                </Button>
+              </div>
+            </>
+          ) : null}
+
+          {transactor.state === "writing" ? <div className="mx-auto">Writing...</div> : null}
+
+          {transactor.state === "waiting" ? (
+            <div className="space-y-2 mx-auto">
+              <p>Waiting for confirmation...</p>
+              <a
+                className="text-blue-600"
+                target="_blank"
+                href={`https://polygonscan.com/tx/${transactor.transactionHash}`}
+                rel="noreferrer"
+              >
+                View on polygonscan
+              </a>
+            </div>
+          ) : null}
+        </div>
+      </Modal>
     </>
   );
 }
