c45f939d68c5164eec70539944dbf63d86d691c4
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2023-05-02 17:23:53 -0400

    wip error page

--- app/components/error-boundary/error.tsx
+++ app/components/error-boundary/error.tsx
@@ -1,14 +1,19 @@
 import { Button } from "../button";
 
-export function Error({ error, children }: { error: Error; children?: React.ReactNode }) {
+export function Error({ error }: { error: Error }) {
   return (
-    <div className="w-full flex items-center justify-center text-gray-800">
-      <div className="max-w-lg space-y-2">
-        <h1 className="text-xl font-semibold">500 Error</h1>
-        <p className="text-xl text-gray-400 pb-4 font-medium">Welp, something broke. This shouldn't be happening </p>
-        <Button className="bg-[#5865F2]">Let us know on Discord</Button>
-        {children}
-      </div>
-    </div>
+    <html lang="en">
+      <body>
+        <div className="w-full flex items-center justify-center text-gray-800">
+          <div className="max-w-lg space-y-2">
+            <h1 className="text-2xl">Ooops!</h1>
+            <p className="text-neutral-400 text-base">something went wrong</p>
+            <img src="/img/error.png" alt="" />
+            <p>It seems you've ventured too deep</p>
+          </div>
+          <Button className="bg-[#5865F2]">Let us know on Discord</Button>
+        </div>
+      </body>
+    </html>
   );
 }
