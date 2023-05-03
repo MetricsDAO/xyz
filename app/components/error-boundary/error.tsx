d4f771521f40c903c56b7ede776df0fffb256929
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2023-05-03 13:43:54 -0400

    Back button

--- app/components/error-boundary/error.tsx
+++ app/components/error-boundary/error.tsx
@@ -4,14 +4,16 @@ export function Error() {
   return (
     <html lang="en">
       <body>
-        <div className="w-full flex flex-col items-center justify-center text-gray-800 gap-5">
+        <div className="w-full flex flex-col items-center justify-center text-gray-800 gap-5 mt-10">
           <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#A0DDA9] to-[#01C2FF]">
             Ooops!
           </h1>
           <p className="text-neutral-400 text-2xl font-bold">something went wrong</p>
           <img src="/img/error.png" alt="" className="my-3" />
           <p className="text-neutral-400 font-medium">It seems you've ventured too deep</p>
-          <Button variant="gradient">Go Back</Button>
+          <Button variant="gradient" onClick={() => history.back()}>
+            Go Back
+          </Button>
         </div>
       </body>
     </html>
