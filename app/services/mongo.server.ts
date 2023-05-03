48bcb903ddfa5b1529f59176c91768d99e847cc8
Hannah Lerman <43100701+thehannahlerman@users.noreply.github.com>
2023-05-03 09:18:21 -0400

    mongo settings

--- app/services/mongo.server.ts
+++ app/services/mongo.server.ts
@@ -6,7 +6,10 @@ import env from "~/env.server";
 import type { SubmissionDoc } from "~/domain/submission/schemas";
 import { pineConfig } from "~/utils/pine-config.server";
 
-const client = new MongoClient(env.MONGODB_URI);
+const client = new MongoClient(env.MONGODB_URI, {
+  connectTimeoutMS: 30000, // increase timeout to 30 seconds
+  maxPoolSize: 200, // set the maximum number of connections in the pool
+});
 
 // Since every index is a deterministic history, we can have each subscriber have its own database.
 // This is useful for deploying changes to the index and having it recreate from scratch.
