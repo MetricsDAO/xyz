8557ecffd8b5c0be4c68ca32f152d135c5f93410
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2023-05-10 10:47:29 -0400

    Form Stepper component and some formatting

--- app/features/service-request-creator/step1-fields.tsx
+++ app/features/service-request-creator/step1-fields.tsx
@@ -1,21 +1,25 @@
 import type { Project } from "@prisma/client";
 import { ClientOnly } from "remix-utils";
-import { Combobox, Error, Field, Input, Select } from "~/components";
+import { Button, Combobox, Error, Field, Input, Progress, Select } from "~/components";
 import { MarkdownEditor } from "~/components/markdown-editor/markdown.client";
 import { ServiceRequestForm, Step1Form, Step1Schema } from "./schema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useNavigate } from "@remix-run/react";
+import { Link, useNavigate } from "@remix-run/react";
 import { Controller, useForm } from "react-hook-form";
 import { useContracts } from "~/hooks/use-root-data";
+import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
+import FormStepper from "~/components/form-stepper/form-stepper";
 
 export function Step1Fields({
   currentData,
   projects,
   onDataUpdate,
+  address,
 }: {
   currentData: Step1Form | null;
   projects: Project[];
   onDataUpdate: (data: Step1Form) => void;
+  address: `0x${string}`;
 }) {
   const contracts = useContracts();
 
@@ -37,83 +41,108 @@ export function Step1Fields({
 
   const onSubmit = (values: Step1Form) => {
     onDataUpdate(values);
-    navigate(`/app/market/new2/step2`);
+    navigate(`/app/market/${address}/request/new/step2`);
   };
 
   return (
-    <>
-      <div className="space-y-2">
-        <h1 className="font-semibold text-3xl mb-2">Launch an Analytics Challenge</h1>
-        <p className="text-lg text-cyan-500">
-          Tap the world’s best Web3 analyst community to deliver quality analytics, tooling, or content that helps
-          projects launch, grow and succeed.
-        </p>
-        <p className="text-sm text-gray-500">
-          Define user permissions, blockchain/project and reward token allowlists, and the reward curve. These
-          parameters will be applied to all challenges in this marketplace
-        </p>
-      </div>
-      <section className="space-y-3">
-        <h2 className="font-bold">Challenge Title*</h2>
-        <Field>
-          <Input {...register("title")} type="text" placeholder="Challenge Title" className="w-full" />
-          <Error error={errors.title?.message} />
-        </Field>
-      </section>
-      <section className="space-y-3">
-        <h2 className="font-bold">What question, problem, or tooling need do you want Web3 analysts to address?*</h2>
-        <ClientOnly>
-          {() => (
-            <div className="container overflow-auto">
-              <MarkdownEditor
-                value={watch("description")}
-                onChange={(v) => {
-                  setValue("description", v ?? "");
-                }}
-              />
-            </div>
-          )}
-        </ClientOnly>
-      </section>
-      <section className="space-y-3">
-        <div className="flex flex-col md:flex-row gap-6 items-start">
-          <div className="flex-grow">
-            <Field>
-              <Controller
-                name="language"
-                control={control}
-                render={({ field }) => {
-                  return (
-                    <Select
-                      {...field}
-                      placeholder="English"
-                      value={"english"}
-                      options={[{ label: "English", value: "english" }]}
-                    />
-                  );
-                }}
-              />
-              <Error error={errors.language?.message} />
-            </Field>
+    <div className="flex">
+      <div className="w-full">
+        <div className="max-w-2xl mx-auto my-10 space-y-10">
+          <div className="space-y-2">
+            <h1 className="font-semibold text-3xl mb-2">Launch an Analytics Challenge</h1>
+            <p className="text-lg text-cyan-500">
+              Tap the world’s best Web3 analyst community to deliver quality analytics, tooling, or content that helps
+              projects launch, grow and succeed.
+            </p>
+            <p className="text-sm text-gray-500">
+              Define user permissions, blockchain/project and reward token allowlists, and the reward curve. These
+              parameters will be applied to all challenges in this marketplace
+            </p>
           </div>
-          <div className="flex-grow">
+          <section className="space-y-3">
+            <h2 className="font-bold">Challenge Title*</h2>
             <Field>
-              <Controller
-                control={control}
-                name="projectSlugs"
-                render={({ field }) => (
-                  <Combobox
-                    {...field}
-                    placeholder="Blockchain/Project"
-                    options={projects.map((p) => ({ label: p.name, value: p.slug }))}
-                  />
-                )}
-              />
-              <Error error={errors.projectSlugs?.message} />
+              <Input {...register("title")} type="text" placeholder="Challenge Title" className="w-full" />
+              <Error error={errors.title?.message} />
             </Field>
+          </section>
+          <section className="space-y-3">
+            <h2 className="font-bold">
+              What question, problem, or tooling need do you want Web3 analysts to address?*
+            </h2>
+            <ClientOnly>
+              {() => (
+                <div className="container overflow-auto">
+                  <MarkdownEditor
+                    value={watch("description")}
+                    onChange={(v) => {
+                      setValue("description", v ?? "");
+                    }}
+                  />
+                </div>
+              )}
+            </ClientOnly>
+          </section>
+          <section className="space-y-3">
+            <div className="flex flex-col md:flex-row gap-6 items-start">
+              <div className="flex-grow">
+                <Field>
+                  <Controller
+                    name="language"
+                    control={control}
+                    render={({ field }) => {
+                      return (
+                        <Select
+                          {...field}
+                          placeholder="English"
+                          value={"english"}
+                          options={[{ label: "English", value: "english" }]}
+                        />
+                      );
+                    }}
+                  />
+                  <Error error={errors.language?.message} />
+                </Field>
+              </div>
+              <div className="flex-grow">
+                <Field>
+                  <Controller
+                    control={control}
+                    name="projectSlugs"
+                    render={({ field }) => (
+                      <Combobox
+                        {...field}
+                        placeholder="Blockchain/Project"
+                        options={projects.map((p) => ({ label: p.name, value: p.slug }))}
+                      />
+                    )}
+                  />
+                  <Error error={errors.projectSlugs?.message} />
+                </Field>
+              </div>
+            </div>
+          </section>
+        </div>
+        <div className="w-full">
+          <Progress progress={25} />
+          <div className="flex items-center justify-evenly">
+            <div className="flex items-center">
+              <button className="flex gap-3 items-center cursor-pointer" type="submit">
+                <p>Next</p>
+                <ArrowRightCircleIcon className="h-8 w-8 text-black" />
+              </button>
+            </div>
+            <div className="flex items-center">
+              <Button className="my-5 mr-4" variant="cancel">
+                <Link to={`/app/market/${address}`}>Cancel</Link>
+              </Button>
+            </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+      <aside className="hidden md:block w-1/6">
+        <FormStepper step={1} labels={["Create", "Analysts", "Reviewers", "Overview"]} />
+      </aside>
+    </div>
   );
 }
