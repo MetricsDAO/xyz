e3a2f867ce95cbadab0512cbc9fc88a4209fd884
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2023-05-17 15:53:28 -0300

    adding osmosis for treasury service' sake

--- app/domain/address.ts
+++ app/domain/address.ts
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
 import { z } from "zod";
 import { PublicKey } from "@solana/web3.js";
+import { bech32 } from "bech32";
 
 export const EvmAddressSchema = z
   .string()
@@ -19,3 +20,13 @@ export const SolAddressSchema = z.string().refine((address) => {
     return false;
   }
 }, "Must be a valid Solana address.");
+
+export const OsmosisAddressSchema = z.string().refine((address) => {
+  try {
+    const { prefix, words } = bech32.decode(address);
+    const result = bech32.encode(prefix, words);
+    return result === address && prefix === "osmo";
+  } catch {
+    return false;
+  }
+}, "Must be a valid Osmosis address.");
