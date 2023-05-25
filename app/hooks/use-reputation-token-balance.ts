43a98d1ffae4cf020ad6c64db6def370703aa0c8
Hannah Lerman <hannahlerman@Hannahs-MBP.fios-router.home>
2023-05-25 14:53:36 -0400

    comment out reputation token balance

--- app/hooks/use-reputation-token-balance.ts
+++ app/hooks/use-reputation-token-balance.ts
@@ -1,4 +1,4 @@
-import { BigNumber } from "ethers";
+import type { BigNumber } from "ethers";
 import { useAccount, useContractRead } from "wagmi";
 import { REPUTATION_TOKEN_ID } from "~/utils/constants";
 import { useContracts } from "./use-root-data";
@@ -8,15 +8,15 @@ import { useContracts } from "./use-root-data";
  * @returns {BigNumber | undefined} - undefined if user is not connected or still loading
  */
 export function useReputationTokenBalance(): BigNumber | undefined {
-  const contracts = useContracts();
-  const { address: userAddress } = useAccount();
-  const { data: reputationBalance } = useContractRead({
-    enabled: !!userAddress,
-    address: contracts.BucketEnforcement.address
-    abi: contracts.BucketEnforcement.abi,
-    functionName: "balanceOf",
-    args: [userAddress as `0x${string}`, BigNumber.from(REPUTATION_TOKEN_ID)],
-  });
+  // const contracts = useContracts();
+  // const { address: userAddress } = useAccount();
+  // const { data: reputationBalance } = useContractRead({
+  //   enabled: !!userAddress,
+  //   address: contracts.BucketEnforcement.address,
+  //   abi: contracts.BucketEnforcement.abi,
+  //   functionName: "marketToMaxScore",
+  //   args: [],
+  // });
 
-  return reputationBalance;
+  return null;
 }
