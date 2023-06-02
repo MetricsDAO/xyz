c26ea5b4797d863bd08fdcb3031bc122a69a6870
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2023-06-02 13:01:00 -0300

    analyst permission form refactor

--- app/routes/app+/market_.new.analyst-permissions.tsx
+++ app/routes/app+/market_.new.analyst-permissions.tsx
@@ -1,16 +1,35 @@
-import { useOutletContext } from "@remix-run/react";
+import { useNavigate, useOutletContext } from "@remix-run/react";
 import { AnalystPermissionsForm } from "~/features/labor-market-creator/analyst-permissions-form";
 import type { OutletContext } from "./market_.new";
 import type { GatingData } from "~/features/labor-market-creator/schema";
+import { FormStepper } from "~/components";
+import { BadgerLinks } from "~/features/labor-market-creator/badger-links";
 
 export default function AnalystPermissionsPage() {
   const [formData, setFormData] = useOutletContext<OutletContext>();
+
+  const navigate = useNavigate();
+
+  const onNext = (values: GatingData) => {
+    setFormData((prevData) => ({ ...prevData, analyst: values }));
+    navigate(`/app/market/new/reviewer-permissions`);
+  };
+
+  const onPrevious = (values: GatingData) => {
+    setFormData((prevData) => ({ ...prevData, analyst: values }));
+    navigate(`/app/market/new/sponsor-permissions`);
+  };
+
   return (
-    <AnalystPermissionsForm
-      currentData={formData.analyst}
-      onDataUpdate={(data: GatingData) => {
-        setFormData((prevData) => ({ ...prevData, analyst: data }));
-      }}
-    />
+    <div className="flex relative min-h-screen">
+      <AnalystPermissionsForm defaultValues={formData.analyst} onNext={onNext} onPrevious={onPrevious} />
+      <aside className="absolute w-1/6 py-28 right-0 top-0">
+        <FormStepper
+          step={3}
+          labels={["Create", "Sponsor Permissions", "Author Permissions", "Reviewer Permissios", "Overview"]}
+        />
+        <BadgerLinks />
+      </aside>
+    </div>
   );
 }
