02e64a58f7cd9f9df3869cbfd1142eaf45774ce1
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2023-06-13 13:33:09 -0400

    Page for viewing - to be deleted

---
+++ app/routes/app+/testing.tsx
@@ -0,0 +1,13 @@
+import { ReviewCreator } from "~/features/review-creator";
+import { ReviewCreatorPanel } from "~/features/review-creator/review-creator-panel";
+
+export default function Testing() {
+  return (
+    /*<ReviewCreator
+      laborMarketAddress="0x0c0e6Fe12C3d1Adda2Da9B70ebDcB136B577E73D"
+      onCancel={}
+      requestId="2464097753396376585514863575769004792702554311071955588692"
+    />*/
+    <ReviewCreatorPanel />
+  );
+}
