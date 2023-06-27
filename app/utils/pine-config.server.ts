08b8ec4f211d9cd6b5a353ac52478c96e6d82fea
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2023-06-27 13:54:38 -0300

    New tracer for new package that better supports claims

--- app/utils/pine-config.server.ts
+++ app/utils/pine-config.server.ts
@@ -6,6 +6,6 @@ export function pineConfig() {
   return {
     namespace: env.ENVIRONMENT === "development" ? "mdao-development" : "mdao-prod",
     subscriber: env.PINE_SUBSCRIBER_OVERRIDE ?? SUBSCRIBER,
-    version: "2.0.4",
+    version: "2.0.9",
   };
 }
