f44b017d2c2616de911e61ecece462394a74caf0
sebruiz <84731260+sebruiz@users.noreply.github.com>
2023-07-05 11:37:56 -0300

    changes for v2 infra (#858)

--- app/utils/pine-config.server.ts
+++ app/utils/pine-config.server.ts
@@ -1,11 +1,11 @@
 import env from "~/env.server";
 
-const SUBSCRIBER = "20230630";
+const SUBSCRIBER = "20230704";
 
 export function pineConfig() {
   return {
     namespace: env.ENVIRONMENT === "development" ? "mdao-development" : "mdao-prod",
     subscriber: env.PINE_SUBSCRIBER_OVERRIDE ?? SUBSCRIBER,
-    version: "2.0.9",
+    version: env.ENVIRONMENT === "development" ? "2.0.9" : "2.1.1",
   };
 }
