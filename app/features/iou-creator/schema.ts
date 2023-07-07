71715845e28406bbe0120148060eab8656b3eaeb
Mason <33527785+masonchain@users.noreply.github.com>
2023-07-07 12:03:57 -0500

    fix: update iou symbol schema

--- app/features/iou-creator/schema.ts
+++ app/features/iou-creator/schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const IOUCreationFormSchema = z.object({
   name: z.string().min(1, "Required"),
-  symbol: z.string().min(1, "Required"),
+  symbol: z.string().min(1, "Required").max(4, "Must be 4 characters or less"),
   destinationChain: z.string().min(1, "Required"),
   destinationAddress: z.string().min(1, "Required"),
 });
