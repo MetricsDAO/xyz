ca6685a99e87a0ed15c4d1f332bead33b12238a3
Morgan TerMaat <morgan.termaat@flipsidecrypto.com>
2023-07-07 11:35:32 -0400

    Add form

--- app/routes/app+/iou-center.tsx
+++ app/routes/app+/iou-center.tsx
@@ -105,34 +105,36 @@ function AddTokenButton({ disabled, networks }: { disabled: boolean; networks: N
         Add Token
       </Button>
       <Modal isOpen={openedCreate} onClose={() => setOpenedCreate(false)} title="Add new Token">
-        <div className="space-y-5 mt-2">
-          <div className="bg-amber-200/10 flex items-center rounded-md p-2">
-            <ExclamationTriangleIcon className="text-yellow-700 mx-2 h-5 w-5" />
-            <p className="text-yellow-700 text-sm">For tokens on polygon only</p>
+        <form>
+          <div className="space-y-5 mt-2">
+            <div className="bg-amber-200/10 flex items-center rounded-md p-2">
+              <ExclamationTriangleIcon className="text-yellow-700 mx-2 h-5 w-5" />
+              <p className="text-yellow-700 text-sm">For tokens on polygon only</p>
+            </div>
+            <Field>
+              <Label>Token Name</Label>
+              <Input label="Token Name" placeholder="Token name" />
+            </Field>
+            <Field>
+              <Label>Token Symbol</Label>
+              <Input label="Token Symbol" placeholder="Symbol" />
+            </Field>
+            <Field>
+              <Label>Contract Address</Label>
+              <Input label="Contract Address" placeholder="contract address" />
+            </Field>
+            <Field>
+              <Label>Decimals</Label>
+              <Input label="Decimals" placeholder="decimals" />
+            </Field>
+            <div className="flex gap-2 justify-end">
+              <Button variant="cancel" onClick={() => setOpenedCreate(false)}>
+                Cancel
+              </Button>
+              <Button disabled={!validAddress}>Save</Button>
+            </div>
           </div>
-          <Field>
-            <Label>Token Name</Label>
-            <Input label="Token Name" placeholder="Token name" />
-          </Field>
-          <Field>
-            <Label>Token Symbol</Label>
-            <Input label="Token Symbol" placeholder="Symbol" />
-          </Field>
-          <Field>
-            <Label>Contract Address</Label>
-            <Input label="Contract Address" placeholder="contract address" />
-          </Field>
-          <Field>
-            <Label>Decimals</Label>
-            <Input label="Decimals" placeholder="decimals" />
-          </Field>
-          <div className="flex gap-2 justify-end">
-            <Button variant="cancel" onClick={() => setOpenedCreate(false)}>
-              Cancel
-            </Button>
-            <Button disabled={!validAddress}>Save</Button>
-          </div>
-        </div>
+        </form>
       </Modal>
     </>
   );
