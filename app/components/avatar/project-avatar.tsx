c6a379fc52502a0a95f2ca1d02f041c84fa07076
Sebastian Ruiz <sebastian@flipsidecrypto.com>
2023-07-13 14:57:48 -0300

    avoid 404s

--- app/components/avatar/project-avatar.tsx
+++ app/components/avatar/project-avatar.tsx
@@ -5,12 +5,14 @@ type Props = {
   project: Pick<Project, "name" | "slug">;
 };
 
+// existing icon files. Avoid 404s.
+const ICONS = ["avalanche", "axelar", "ethereum", "flow", "matic", "near", "polygon", "solana"];
+
 export function ProjectAvatar({ project }: Props) {
-  return (
-    <Avatar
-      src={`/img/icons/project-icons/${project.slug}.svg`}
-      alt={`${project.name} logo`}
-      fallback={project.slug.at(0)?.toUpperCase()}
-    />
-  );
+  let src;
+  if (ICONS.includes(project.slug)) {
+    src = `/img/icons/project-icons/${project.slug}.svg`;
+  }
+
+  return <Avatar src={src} alt={`${project.name} logo`} fallback={project.slug.at(0)?.toUpperCase()} />;
 }
