ffb9bb603bff5d83d52d2809150e16e98bcf2fd8
⚪ CHANCE <chance@utc24.io>
2023-09-06 13:17:33 -0500

    fix: type change

--- app/utils/pine-config.server.ts
+++ app/utils/pine-config.server.ts
@@ -6,6 +6,6 @@ export function pineConfig() {
   return {
     namespace: env.ENVIRONMENT === "development" ? "mdao-development" : "mdao-prod",
     subscriber: env.PINE_SUBSCRIBER_OVERRIDE ?? SUBSCRIBER,
-    version: env.ENVIRONMENT === "development" ? "2.0.9" : "2.2.0",
+    version: env.ENVIRONMENT === "development" ? "2.2.0" : "2.2.0",
   };
 }
